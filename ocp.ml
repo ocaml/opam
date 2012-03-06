c56242680da0eae3104f0d30c2f799771311d513
Frédéric Tuong <frederic.tuong@inria.fr>
2012-03-06 17:49:05 +0100

    The upload accepts a regular file or directory in case the given is not a targz.

--- ocp.ml
+++ ocp.ml
@@ -15,11 +15,18 @@ struct
 
   let table = ref (Debian.Debcudf.init_tables [])
 
-  let version_of_string n version = { deb = version ; cudf = Debian.Debcudf.get_cudf_version !table (n, version) }
+  let version_of_string n version = 
+    { deb = version 
+    ; cudf = 
+        match try Some (Debian.Debcudf.get_cudf_version !table (n, version)) with Not_found -> None with
+          | Some v -> v
+          | None -> int_of_string version }
 
   let nv_of_string s = 
     let n, version = BatString.split s "-" in
       Name n, version_of_string n version
+
+  let default_version = "0"
 end
 
 type name_version = Namespace.name * Namespace.version
@@ -81,7 +88,7 @@ sig
   (** definitions of some shortcuts *)
   val root : filename (* / *)
     (** the root of every path *)
-  val package : t -> filename (* $PWD *)
+  val package : t -> string (* calculated from $PWD *) -> filename
     (** path in the packager filesystem, contains the collection of libraries and programs *)
   val lib : t -> Namespace.name -> filename (* $HOME_OPAM_OVERSION/lib/NAME *)
     (** installed libraries for the package (at most one version installed) *)
@@ -119,6 +126,9 @@ sig
   val extract_targz : t -> binary_data archive -> binary_data contents_rec
     (** Returns the same meaning as [archive] but in extracted form. *)
 
+  val raw_targz : filename -> binary_data archive
+    (** Considers the given [filename] as the contents of an [archive] already extracted. *)
+
   val exec_buildsh : t -> name_version -> t
     (* $HOME_OPAM/build/NAME-VERSION/build.sh *)
     (** Executes this particularly named script. *)
@@ -129,8 +139,9 @@ sig
   val basename : filename -> basename
     (** see [Filename.basename] *)
 
-  val chop_extension : basename -> string
-    (** see [Filename.chop_extension] *)
+  val nv_of_extension : string (* version *) -> basename -> Namespace.name * Namespace.version
+    (** see [Filename.chop_extension], but in case of no extensions, it behaves as the identity function. 
+        When [basename] is not of the form "NAME-VERSION", or when we can not extract the version, [string] is returned as version. *)
 
   val concat : filename -> basename -> filename
     (** see [Filename.concat] *)
@@ -198,7 +209,7 @@ struct
     { computer = o ; home ; home_ocamlversion = home // ocamlv }
 
   let root = Raw "/"
-  let package _ = normalize "."
+  let package _ s = Raw (Printf.sprintf "%s" s)
   let lib t (Namespace.Name n) = Raw (t.home_ocamlversion // "lib" // n)
   let bin t = Raw (t.home_ocamlversion // "bin")
 
@@ -239,11 +250,20 @@ struct
       (fun fic -> File ((*Binary (BatFile.with_file_in fic BatIO.read_all)*)Filename fic))
       Not_exists
 
-  let chop_extension (B s) = Filename.chop_extension s
+  let nv_of_extension version (B s) = 
+    let s = 
+      match try Some (Filename.chop_extension s) with _ -> None with 
+        | Some s -> s
+        | _ -> s in
+
+    match try Some (Namespace.nv_of_string s) with _ -> None with
+      | Some nv -> nv
+      | None -> Namespace.Name s, Namespace.version_of_string s version
+
   let file_exists f = Sys.file_exists (s_of_filename f)
 
   let index_opam_list t =
-    BatList.map (fun fic -> Namespace.nv_of_string (chop_extension fic))
+    BatList.map (nv_of_extension Namespace.default_version)
       (match find t (index_opam t None) with
         | Directory l -> l
         | _ -> [])
@@ -259,16 +279,51 @@ struct
     let () = aux (s_of_filename f) in
     t
 
-  let add t f =
-    function 
+  module U = 
+  struct
+    let mkdir = 
+      let rec aux f_to = 
+        if Sys.file_exists f_to then
+          ()
+        else
+          begin
+            aux (Filename.dirname f_to);
+            Unix.mkdir f_to 0o755;
+          end in
+      aux
+
+    let link f_from f_to = 
+      let () = mkdir (Filename.dirname f_to) in
+      Unix.link f_from f_to
+  end
+
+  let add t f = function 
       | Directory d -> failwith "to complete !"
       | File (Binary cts) -> 
           let () = contents (fun _ -> failwith "to complete !") Unix.unlink () t f in
           let fic = s_of_filename f in
           let () = BatFile.with_file_out fic (fun oc -> BatString.print oc cts) in
-            t
-      | File (Filename fic) -> failwith "to complete ! copy the given filename"
-      | Not_exists -> failwith "to complete !"
+          t
+      | File (Filename fic) -> 
+      let () = 
+        match (Unix.lstat fic).Unix.st_kind with
+          | Unix.S_DIR -> 
+            let () = contents (fun _ -> ()) (fun _ -> failwith "to complete !") () t f in
+            let rec aux f_from f_to = 
+              (match (Unix.lstat f_from).Unix.st_kind with
+                | Unix.S_DIR -> List.fold_left (fun _ b -> aux (f_from // b) (f_to // b)) () (BatSys.files_of f_from)
+                | Unix.S_REG -> 
+                  let () = 
+                    if Sys.file_exists f_to then
+                      Unix.unlink f_to
+                    else
+                      () in
+                  U.link f_from f_to
+                | _ -> failwith "to complete !") in
+            aux fic (s_of_filename f)
+          | _ -> Printf.kprintf failwith "to complete ! copy the given filename %s" fic in
+      t
+    | Not_exists -> t
 
   let compare_computer t1 t2 = compare t1.computer t2.computer
 
@@ -282,6 +337,8 @@ struct
     | Tar_gz (Binary _) -> failwith "to complete ! check if the \"dose\" project has been configured with the correct option to extract the gzip or bz2, then use similars functions to extract" (*IO.read_all (Common.Input.open_file fic)*)
     | Tar_gz (Filename fic) -> R_filename [Raw fic]
     | Empty -> R_directory []
+
+  let raw_targz f = Tar_gz (Filename (s_of_filename f))
 
   let lstat s = Unix.lstat (s_of_filename s)
   let files_of f = BatSys.files_of (s_of_filename f)
@@ -321,13 +378,7 @@ struct
 
   let string_of_url (U s) = s
 end
-
-let filename_of_string s = 
-  List.fold_left 
-    (fun t s -> Path.concat t (B s)) 
-    Path.root
-    (BatString.nsplit (BatString.strip ~chars:"/" s) "/")
-    
+  
 module File =
 struct
   open Namespace
@@ -784,7 +835,7 @@ struct
       List.fold_left
         (fun map x -> 
            NV_map.add
-             (Namespace.nv_of_string (Path.chop_extension x)) 
+             (Path.nv_of_extension Namespace.default_version x) 
              (F_cudf.package (F_cudf.find home (Path.concat archives x))) 
              map) NV_map.empty 
         (match Path.find home archives with
@@ -825,9 +876,9 @@ struct
           Path.add 
             t.home 
             (Path.archives_targz t.home (Some n_v)) 
-            (Path.File (match arch with 
-                          | Empty -> failwith "create an empty tar.gz here" 
-                          | Tar_gz s -> s)) } in
+            (match arch with 
+              | Empty -> Path.Not_exists
+              | Tar_gz s -> Path.File s) } in
       
     match o_pack with
       | None -> { t with current_repository = NV_map.add n_v (F_cudf.new_package n_v "") t.current_repository }
@@ -865,7 +916,7 @@ sig
     (** Finds a consistent state where most of the installed packages are
         upgraded to their latest version. *)
 
-  val upload : t -> Path.filename -> t
+  val upload : t -> string -> t
     (** Sends a new created package to the server. *)
 
   val remove : t -> Namespace.name -> t
@@ -1157,14 +1208,25 @@ struct
       resolve t (Path.index_opam_list t.home) 
         { Solver.wish_install = [] ; wish_remove = [] ; wish_upgrade = BatList.map vpkg_of_nv (F_installed.find t.home (Path.installed t.home)) }
     
-  let upload t filename = 
-    { t with
-      server = 
-        Server.newArchive t.server
-          (Server.getOpam t.server (Namespace.nv_of_string (Path.chop_extension (Path.basename filename))))
-          (match Path.find t.home filename with
-             | Path.File binary -> Tar_gz binary
-             | _ -> Empty) }
+  let upload t s_filename = 
+    let filename = Path.package t.home s_filename in
+    let t, o =
+      let f msg v = 
+        let ok, t = confirm t (Printf.sprintf "Path \"%s\" %s. It will be uploaded anyway." s_filename msg) in
+        if ok then
+          t, Some v
+        else
+          t, None in
+      match Path.find t.home filename with
+        | Path.File binary -> t, Some (Tar_gz binary)
+        | Path.Directory _ -> f "is a directory" (Path.raw_targz filename)
+        | Path.Not_exists -> f "has not been found" Empty in
+
+    match o with
+      | Some v ->
+        { t with server = 
+            Server.newArchive t.server (Server.getOpam t.server (Path.nv_of_extension Namespace.default_version (Path.basename filename))) v }
+      | None -> t
 
   type config_request = Dir
   let config t Dir name =     
@@ -1530,7 +1592,13 @@ end
 module C = M.C
 
 open Namespace
-
+(*
+let filename_of_string s = 
+  List.fold_left 
+    (fun t s -> Path.concat t (B s)) 
+    Path.root
+    (BatString.nsplit (BatString.strip ~chars:"/" s) "/")
+*)
 let _ = 
   let client = C.init0 () in
   let f x = 
@@ -1557,7 +1625,7 @@ let _ =
           
         | "upgrade" :: _ -> C.upgrade client
           
-        | "upload" :: s :: _ -> C.upload client (filename_of_string s)
+        | "upload" :: s :: _ -> C.upload client s
           
         | "remove" :: name :: _ -> C.remove client (Name name)
           
