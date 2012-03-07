1d33c077529341aa626e03a01182665bfc5a48ae
Thomas Gazagnaire <thomas@gazagnaire.org>
2012-03-07 16:47:02 +0100

    As the server forks a new process on each incoming message, the "incr request" trick does not work (processes do not share the same memory).
    I'm using another trick for now on to get almost unique idents.

--- src/ocp_get_server.ml
+++ src/ocp_get_server.ml
@@ -41,32 +41,31 @@ let server fn =
 
   establish_server (fn state) addr
 
-let request = ref 0
-
-let log fmt =
-  Globals.log (Printf.sprintf "REQUEST-%d" !request) fmt
+let log id fmt =
+  Globals.log (Printf.sprintf "REQUEST [%d]" id) fmt
 
 let protect f =
   try f ()
   with e -> Oerror (Printexc.to_string e)
 
 let fn t stdin stdout =
-  incr request;
-  log "Processing an incoming request";
+  Random.self_init();
+  let id = Random.int 1024 in
+  log id "Processing an incoming request";
 
   let output = match (input_value stdin : input_api) with
   | IgetList                    ->
-      log "getList";
+      log id "getList";
       protect (fun () -> OgetList (Server.getList t))
   | IgetOpam name_version       ->
-      log "getOpam";
+      log id "getOpam";
       protect (fun () -> OgetOpam (Server.getOpam t name_version))
   | IgetArchive opam            ->
-      log "getArchive";
+      log id "getArchive";
       protect (fun () -> OgetArchive (Server.getArchive t opam))
   | InewArchive (opam, archive) ->
       (* XXX: need to protect the server state mutation as it can be updated concurrently *)
-      log "newArchive";
+      log id "newArchive";
       protect (fun () -> Server.newArchive t opam archive; OnewArchive) in 
 
   output_value stdout output;
