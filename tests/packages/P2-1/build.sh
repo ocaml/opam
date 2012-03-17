6b5bbb2b2a6cc7be258225d2c1d39c45350cfbcc
Thomas Gazagnaire <thomas@gazagnaire.org>
2012-03-17 15:14:29 +0100

    [test] fix compilation of P2
    now, when installing P3, it asks to uninstall P2 ...

--- tests/packages/P2-1/build.sh
+++ tests/packages/P2-1/build.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
-OCPGET="./ocp-get --root /tmp/OPAM.TEST"
+
+OCPGET="ocp-get --root /tmp/OPAM.TEST"
+FLAGS=`${OCPGET} config dir P1`
 
 echo "Bytecode Compilation"
-ocamlopt -c `${OCPGET} config dir P1` p2.ml
+ocamlopt -c ${FLAGS} p2.ml
 
 echo "Native Compilation"
-ocamlc -c `${OCPGET} config dir P1` p2.ml
+ocamlc -c ${FLAGS} p2.ml
