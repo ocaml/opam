89e8e1091227c5309645d44372e2a8eab4794c64
Frédéric Tuong <frederic.tuong@inria.fr>
2012-04-23 16:03:44 +0200

    Parallel building works. It is activated by default. Notes : - By default, 4 cores are considered. - Unix.fork and Unix.wait have not been tested on every platform. - Because of parallelism, output messages in the terminal are unordered, it becomes hard to follow what the package manager is doing.

--- src/run.ml
+++ src/run.ml
@@ -81,7 +81,62 @@ sig
   val filter_finished : ('a, 'b) t -> (state * 'a) plist (** By convention : list not empty *) -> ('a, 'b) t * (state * 'a) plist * ('a * 'b return (* finished *))
 end
 
-module Process : PROCESS with type 'a return = 'a =
+module Process : PROCESS with type 'a return = unit =
+struct
+  open BatMap
+
+  type 'a return = unit
+  type ('a, 'b) t = { cores_max : int ; running : int ; f : 'a -> unit ; to_str : 'a -> string }
+
+  type 'a plist = 'a list (* order irrelevant *)
+
+  type state = 
+    | Has_began of int
+    | Not_yet_begun
+
+  let cores = 4 (* TODO compute the number of processors of the computer *)
+
+  let zero_running = 0
+
+  let init cores_max f to_str = { cores_max = max 1 cores_max ; running = zero_running ; f ; to_str } 
+
+  module Unix2 =
+  struct
+    include Unix
+
+    (* the [Unix.wait] could return a processus which has not been created by [Unix.fork]. This part waits until a known pid is returned. *)
+    let wait map_pid = 
+      let rec aux () = 
+        let pid, error = Unix.wait () in
+        if IntMap.mem pid map_pid then
+          pid, error
+        else
+          aux () in
+      aux ()
+  end
+
+  (* This function always execute in parallel. *)
+  let filter_finished t l = 
+    let rec aux map_pid = function
+      | 0 as nb, l | nb, ([] as l) -> nb, l, map_pid
+      | nb, (Has_began pid, _ as proc) :: xs -> aux (IntMap.add pid proc map_pid) (nb, xs)
+      | nb, (Not_yet_begun, x) :: xs -> 
+          match Unix.fork () with
+            | 0 -> let () = t.f x in exit 0
+            | pid -> aux (IntMap.add pid (Has_began pid, x) map_pid) (pred nb, xs) in
+
+    let nb_remained, l_not_begun, map_pid = aux IntMap.empty (t.cores_max - t.running, l) in
+    let pid, error = Unix2.wait map_pid in
+    let _, x = IntMap.find pid map_pid in
+    match error with
+      | Unix.WEXITED 0 -> 
+          { t with running = max zero_running (t.running + pred nb_remained) }, 
+          IntMap.values (IntMap.remove pid map_pid) @ l_not_begun, 
+          (x, ())
+      | _ -> Globals.error_and_exit "command %s failed" (t.to_str x)
+end
+
+module Process_single : PROCESS with type 'a return = 'a =
 struct
   type ('a, 'b) t = { nb_proc : int ; f : 'a -> 'b ; to_str : 'a -> string }
 
