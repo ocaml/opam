5d7dc00e1b54fd3e97dd7b54fb4928a01ec1d0d5
Thomas Gazagnaire <thomas@gazagnaire.org>
2012-05-25 13:04:27 +0200

    Remove debug messages

--- src/repo/rsync/update.ml
+++ src/repo/rsync/update.ml
@@ -12,7 +12,7 @@ let remote_address = Sys.argv.(1)
 open Types
 let (/) = Stdlib_filename.concat
 
-let log fmt = Globals.log "opam-rsync-update" fmt
+let log = Globals.log "opam-rsync-update"
 
 let rsync ?fn dir =
   let option, filter = match fn with
@@ -26,18 +26,12 @@ let rsync ?fn dir =
   List.iter (fun x -> log "updated: %s" (NV.to_string x)) files;
   List.fold_left (fun set f -> NV.Set.add f set) NV.Set.empty files
 
-let _ =    log "FOO1"
-
 let () =
   let fn str = NV.of_filename (Filename.of_string str) in
-    log "FOO2";
   let opam = rsync ~fn "opam/" in
-    log "FOO3";
   let descr = rsync "descr/" in
   let archives =
-    log "FOO";
     let files = Run.files "archives" in
-    log "BAR";
     List.fold_left (fun set f -> NV.Set.union (rsync ~fn f) set) NV.Set.empty files in
   let updates = NV.Set.union archives (NV.Set.union opam descr) in
   File.Updated.write
