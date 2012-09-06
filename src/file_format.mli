4ca3c23e5bf2aa6c8d3f7ff014565bd10811d1e6
Thomas Gazagnaire <thomas@gazagnaire.org>
2012-09-06 18:31:32 +0200

    Remove TypeRex from headers
    Fix #83

--- src/file_format.mli
+++ src/file_format.mli
@@ -6,7 +6,7 @@
 (*  All rights reserved.  This file is distributed under the terms of  *)
 (*  the GNU Public License version 3.0.                                *)
 (*                                                                     *)
-(*  TypeRex is distributed in the hope that it will be useful,         *)
+(*  OPAM is distributed in the hope that it will be useful,            *)
 (*  but WITHOUT ANY WARRANTY; without even the implied warranty of     *)
 (*  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the      *)
 (*  GNU General Public License for more details.                       *)
