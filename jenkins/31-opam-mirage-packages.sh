67c03d86d9ca1e0b04de77c0fbec3608b4a91720
Anil Madhavapeddy <anil@recoil.org>
2012-10-10 22:16:24 +0100

    sync jenkins scripts

--- jenkins/31-opam-mirage-packages.sh
+++ jenkins/31-opam-mirage-packages.sh
@@ -1,7 +1,7 @@
 #!/bin/sh -ex
 PREFIX=$1
 OPAM=$HOME/opam-bin/$PREFIX/bin/opam
-ROOT=`echo /b/${JOB_NAME} | sed -e s,=,_,g`
+ROOT=`echo /x/${JOB_NAME} | sed -e "s,=,_,g" -e "s/,/-/g"`
 rm -rf ${ROOT}
 $OPAM --yes --root $ROOT init .
 $OPAM --yes --root $ROOT remote -add dev git://github.com/mirage/opam-repo-dev
@@ -11,4 +11,4 @@ fi
 if [ "${packages}" = "all" ]; then
   packages=`$OPAM --root $ROOT list -short`
 fi
-$OPAM --yes --root $ROOT install ${packages}
+$OPAM --verbose --yes --root $ROOT install ${packages}
