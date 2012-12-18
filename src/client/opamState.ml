6b2401d08f1d818104512305c4ee426f6e61bcd8
Thomas Gazagnaire <thomas@gazagnaire.org>
2012-12-18 11:02:35 +0100

    Really fix 'opam update' regression (continue 096ca3bac5f4de27ad9c9a846b1ee18c771b694b)

--- src/client/opamState.ml
+++ src/client/opamState.ml
@@ -768,9 +768,9 @@ let install_compiler t ~quiet switch compiler =
 
 let update_pinned_package t nv pin =
   match kind_of_pin_option pin with
-  | (`git|`local) ->
+  | (`git|`local as k) ->
     let path = OpamFilename.raw_dir (path_of_pin_option pin) in
-    let module B = (val OpamRepository.find_backend `git: OpamRepository.BACKEND) in
+    let module B = (val OpamRepository.find_backend k: OpamRepository.BACKEND) in
     let build = OpamPath.Switch.build t.root t.switch nv in
     B.download_dir nv ~dst:build path
   | _ ->
