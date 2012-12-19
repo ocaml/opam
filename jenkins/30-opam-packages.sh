0f85da6a5b7406a644a39fe5590da59938f10201
Anil Madhavapeddy <anil@recoil.org>
2012-12-19 10:12:45 +0000

    more fixes to cmdliner jenkins

--- jenkins/30-opam-packages.sh
+++ jenkins/30-opam-packages.sh
@@ -12,6 +12,7 @@ esac
 
 export OPAMROOT=$ROOT
 export OPAMYES=1
+export OPAMVERBOSE=1
 
 $OPAM init $2
 $OPAM config list
@@ -22,8 +23,8 @@ system)
 system4)
   ;;
 *)
-  $OPAM $ROOT switch ${compiler}
+  $OPAM switch ${compiler}
   ;;
 esac
 
-$OPAM --verbose install ${packages}
+$OPAM install ${packages}
