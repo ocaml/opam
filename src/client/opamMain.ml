af7b0f9c79fc73b8431ad7a7ab82b3b9b7220ae1
Thomas Gazagnaire <thomas@gazagnaire.org>
2013-01-07 11:45:47 +0100

    Remove the "-c" short-cut to "--comp" because "-comp" is then understood to "-c omp" which is quite ambiguous

--- src/client/opamMain.ml
+++ src/client/opamMain.ml
@@ -291,7 +291,7 @@ let init =
   ] in
   let cores = mk_opt ["j";"jobs"] "JOBS" "Number of jobs to use when building packages." Arg.int 1 in
   let compiler =
-    mk_opt ["c";"comp"] "VERSION" "Which compiler version to use." compiler OpamCompiler.default in
+    mk_opt ["comp"] "VERSION" "Which compiler version to use." compiler OpamCompiler.default in
   let repo_name =
     let doc = Arg.info ~docv:"NAME" ~doc:"Name of the repository." [] in
     Arg.(value & pos ~rev:true 1 repository_name OpamRepositoryName.default & doc) in
