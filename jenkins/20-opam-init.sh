f811688469fcc4ad37d99c09cde5adfe8c14a588
Thomas Gazagnaire <thomas@gazagnaire.org>
2013-01-30 12:40:48 +0000

    Fix jenkins init

--- jenkins/20-opam-init.sh
+++ jenkins/20-opam-init.sh
@@ -1,7 +1,7 @@
 #!/bin/sh -ex
 PREFIX=$1
 if [ "$PREFIX" = "" ]; then echo Need prefix as first arg; exit 1; fi
-make distclean
+cd src_ext && make distclean
 ./configure --prefix=$HOME/opam-bin/$PREFIX
 make
 make install
