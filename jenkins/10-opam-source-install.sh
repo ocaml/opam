f82e73a294a5f011414b014078b935aa1e765e10
Anil Madhavapeddy <anil@recoil.org>
2013-02-13 09:59:17 +0000

    jenkins: manual libsrs install (deb missing from wheezy)

--- jenkins/10-opam-source-install.sh
+++ jenkins/10-opam-source-install.sh
@@ -8,3 +8,12 @@ env CFLAGS="-fPIC" ./configure && make
 sudo make install
 cd ..
 rm -rf nlopt-2.3
+
+rm -f libsrs2-1.0.18.tar.gz
+curl -OL http://www.libsrs2.org/srs/libsrs2-1.0.18.tar.gz
+tar -zxvf libsrs2-1.0.18.tar.gz
+cd libsrs2-1.0.18
+./configure && make
+sudo make install
+cd ..
+rm -rf libsrs2-1.0.18
