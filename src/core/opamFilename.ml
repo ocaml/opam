ocaml/opam

commit de774165518c9c2abdeaf63a68081738fce1e774

Expand ~ in filename strings
This fixes `opam config setup --dot-profile=~/.bashrc`

Remark: this was already working fine if you do `opam config setup --dot-profile ~/.bashrc` (as this was shell-expanded)

src/core/opamFilename.ml
@@ -22,10 +22,18 @@ module Dir = struct
   include OpamMisc.Base
 
   let of_string dirname =
-    if not (Filename.is_relative dirname) then
-      dirname
-    else
+    if (String.length dirname > 1 && dirname.[0] = '~') then
+      let home = OpamMisc.getenv "HOME" in
+      match dirname with
+      | "~" -> home
+      | _   ->
+        let prefix = Filename.concat "~" "" in
+        let suffix = OpamMisc.remove_prefix ~prefix dirname in
+        Filename.concat home suffix
+    else if not (Filename.is_relative dirname) then
       OpamSystem.real_path dirname
+    else
+      dirname
 
   let to_string dirname =
     if dirname.[String.length dirname - 1] = Filename.dir_sep.[0] then

commit 6324e18e24c2f58feea6cf555d59206780b4fe1c

Fix silly regression introduced in de774165518c9c2abdeaf63a68081738fce1e774

src/core/opamFilename.ml
@@ -30,7 +30,7 @@ module Dir = struct
         let prefix = Filename.concat "~" "" in
         let suffix = OpamMisc.remove_prefix ~prefix dirname in
         Filename.concat home suffix
-    else if not (Filename.is_relative dirname) then
+    else if Filename.is_relative dirname then
       OpamSystem.real_path dirname
     else
       dirname