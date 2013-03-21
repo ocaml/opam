ocaml/opam

commit a7ec825f8dbc3c7a3fcb7a91617c0d623105125e

Use List.rev_map instead of List.map when needed.

src/client/opamClient.ml
@@ -532,28 +532,28 @@ module API = struct
         OpamPackage.Name.Set.of_list (OpamPackage.Name.Map.keys t.pinned)
       else
         let names =
-          List.map (OpamRepositoryName.to_string |> OpamPackage.Name.of_string) repos in
+          List.rev_map (OpamRepositoryName.to_string |> OpamPackage.Name.of_string) repos in
         OpamPackage.Name.Set.of_list (List.filter (OpamState.is_pinned t) names) in
     let pinned_packages_need_update =
       not (OpamPackage.Name.Set.is_empty pinned_packages) in
 
     let valid_repositories =
       OpamMisc.StringSet.of_list
-        (List.map OpamRepositoryName.to_string
+        (List.rev_map OpamRepositoryName.to_string
            (OpamRepositoryName.Map.keys repositories)) in
     let valid_pinned_packages =
       OpamMisc.StringSet.of_list
-        (List.map OpamPackage.Name.to_string
+        (List.rev_map OpamPackage.Name.to_string
            (OpamPackage.Name.Map.keys t.pinned)) in
     let unknown_names, not_pinned =
       if repos = [] then
         [], []
       else
         let all =
-          OpamMisc.StringSet.of_list (List.map OpamRepositoryName.to_string repos) in
+          OpamMisc.StringSet.of_list (List.rev_map OpamRepositoryName.to_string repos) in
         let valid_names =
           OpamMisc.StringSet.of_list
-            (List.map
+            (List.rev_map
                (OpamPackage.name |> OpamPackage.Name.to_string)
                (OpamPackage.Set.elements t.packages)) in
         let (--) = OpamMisc.StringSet.diff in
@@ -724,7 +724,7 @@ module API = struct
       log "installing compiler packages";
       let t = OpamState.load_state "init-2" in
       let compiler_packages = OpamState.get_compiler_packages t compiler in
-      let compiler_names = OpamPackage.Name.Set.of_list (List.map fst compiler_packages) in
+      let compiler_names = OpamPackage.Name.Set.of_list (List.rev_map fst compiler_packages) in
       let _solution = OpamSolution.resolve_and_apply ~force:true t (Init compiler_names)
           { wish_install = [];
             wish_remove  = [];
@@ -750,7 +750,7 @@ module API = struct
     log "INSTALL %s" (OpamPackage.Name.Set.to_string names);
     let t = OpamState.load_state "install" in
     let atoms = OpamSolution.atoms_of_names t names in
-    let names = OpamPackage.Name.Set.of_list (List.map fst atoms) in
+    let names = OpamPackage.Name.Set.of_list (List.rev_map fst atoms) in
 
     let pkg_skip, pkg_new =
       List.partition (fun (n,v) ->
@@ -785,7 +785,7 @@ module API = struct
     if t.installed_roots <> current_roots then (
       let diff = OpamPackage.Set.diff t.installed_roots current_roots in
       let diff = OpamPackage.Set.elements diff in
-      let diff = List.map OpamPackage.to_string diff in
+      let diff = List.rev (List.rev_map OpamPackage.to_string diff) in
       OpamGlobals.msg
         "Adding %s to the list of installed roots.\n"
         (OpamMisc.pretty_list diff);
@@ -889,7 +889,9 @@ module API = struct
     );
 
     if autoremove || atoms <> [] then (
-      let packages = OpamPackage.Set.of_list (List.map (fun (n,_) -> OpamState.find_installed_package_by_name t n) atoms) in
+      let packages =
+        OpamPackage.Set.of_list
+          (List.rev_map (fun (n,_) -> OpamState.find_installed_package_by_name t n) atoms) in
       let universe = OpamState.universe t Depends in
       let to_remove =
         OpamPackage.Set.of_list
@@ -950,8 +952,7 @@ module API = struct
     let depends =
       let universe = OpamState.universe t Depends in
       OpamSolver.reverse_dependencies ~depopts:true ~installed:true universe reinstall in
-    let to_process =
-      List.map (fun pkg -> To_recompile pkg) (List.rev depends) in
+    let to_process = List.rev_map (fun pkg -> To_recompile pkg) depends in
     let solution = OpamSolution.apply t Reinstall (OpamSolver.sequential_solution to_process) in
     OpamSolution.check_solution solution
 

src/client/opamSolution.ml
@@ -33,13 +33,13 @@ let eq_atom name version =
   name, Some (`Eq, version)
 
 let eq_atoms_of_packages set =
-  List.map (fun nv -> eq_atom (OpamPackage.name nv) (OpamPackage.version nv)) (OpamPackage.Set.elements set)
+  List.rev_map (fun nv -> eq_atom (OpamPackage.name nv) (OpamPackage.version nv)) (OpamPackage.Set.elements set)
 
 let atom_of_package nv =
   OpamPackage.name nv, None
 
 let atoms_of_packages set =
-  List.map atom_of_package (OpamPackage.Set.elements set)
+  List.rev_map atom_of_package (OpamPackage.Set.elements set)
 
 let atom_of_name name =
   name, None
@@ -71,7 +71,7 @@ let atoms_of_names t names =
       OpamPackage.unavailable_because_pinned name version
     else
       OpamPackage.unavailable name version in
-  List.map
+  List.rev_map
     (fun name ->
       if exists name None then
         if available name None then
@@ -118,8 +118,8 @@ let display_error (n, error) =
 
 (* Prettify errors *)
 let string_of_errors errors =
-  let actions = List.map fst errors in
-  let packages = List.map action_contents actions in
+  let actions = List.rev_map fst errors in
+  let packages = List.rev_map action_contents actions in
   match packages with
   | []  -> assert false
   | [h] -> OpamPackage.to_string h
@@ -128,7 +128,7 @@ let string_of_errors errors =
 
 let new_variables e =
   let e = List.filter (fun (_,s,_) -> s="=") e in
-  let e = List.map (fun (v,_,_) -> v) e in
+  let e = List.rev_map (fun (v,_,_) -> v) e in
   OpamMisc.StringSet.of_list e
 
 let variable_warnings = ref false
@@ -222,7 +222,7 @@ let parallel_apply t action solution =
 
   let root_installs =
     let names =
-      List.map OpamPackage.name (OpamPackage.Set.elements t.installed_roots) in
+      List.rev_map OpamPackage.name (OpamPackage.Set.elements t.installed_roots) in
     OpamPackage.Name.Set.of_list names in
   let root_installs = match action with
     | Init r

src/client/opamState.ml
@@ -482,7 +482,7 @@ let switch_consistency_checks t =
     clean pin_dir name in
   let available =
       let dirs = OpamFilename.list_dirs pin_cache in
-      let pkgs = List.map (
+      let pkgs = List.rev_map (
           OpamFilename.basename_dir
           |> OpamFilename.Base.to_string
           |> OpamPackage.Name.of_string
@@ -731,9 +731,9 @@ let contents_of_variable t v =
       let names = OpamMisc.split name_str '+' in
       if List.length names = 1 then
         OpamGlobals.error_and_exit "Package %s is not installed" name_str;
-      let names = List.map OpamPackage.Name.of_string names in
+      let names = List.rev_map OpamPackage.Name.of_string names in
       let results =
-        List.map (fun name ->
+        List.rev_map (fun name ->
           match process_one name with
           | None   -> OpamGlobals.error_and_exit "Package %s is not installed" (OpamPackage.Name.to_string name)
           | Some r -> r
@@ -823,7 +823,7 @@ let filter_commands t l =
   OpamMisc.filter_map (filter_command t) l
 
 let expand_env t (env: env_updates) : env =
-  List.map (fun (ident, symbol, string) ->
+  List.rev_map (fun (ident, symbol, string) ->
     let string = substitute_string t string in
     let read_env () =
       let prefix = OpamFilename.Dir.to_string t.root in
@@ -922,7 +922,7 @@ let string_of_env_update t updates =
       | "=+" -> (ident, Printf.sprintf "$%s:%s" ident string)
       | _    -> failwith (Printf.sprintf "%s is not a valid env symbol" symbol) in
     Printf.sprintf "%s=%s\n" key value in
-  String.concat "" (List.map aux updates)
+  String.concat "" (List.rev_map aux updates)
 
 let init_script t ~switch_eval ~complete =
   let variables =
@@ -1220,7 +1220,7 @@ let install_conf_ocaml_config root switch =
   log "install_conf_ocaml_config switch=%s" (OpamSwitch.to_string switch);
   (* .config *)
   let vars =
-    let map f l = List.map (fun (s,p) -> OpamVariable.of_string s, S (f p)) l in
+    let map f l = List.rev_map (fun (s,p) -> OpamVariable.of_string s, S (f p)) l in
     let id x = x in
 
     map OpamFilename.Dir.to_string

src/core/opamFile.ml
@@ -87,7 +87,9 @@ module Filenames = struct
 
   let to_string _ s =
     let lines =
-      List.map (fun f -> [OpamFilename.to_string f]) (OpamFilename.Set.elements s) in
+      List.rev_map
+        (fun f -> [OpamFilename.to_string f])
+        (OpamFilename.Set.elements s) in
     Lines.to_string lines
 
 end
@@ -110,7 +112,9 @@ module Urls_txt = struct
 
   let to_string _ t =
     let lines =
-      List.map (fun r -> [OpamFilename.Attribute.to_string r]) (OpamFilename.Attribute.Set.elements t) in
+      List.rev_map
+        (fun r -> [OpamFilename.Attribute.to_string r])
+        (OpamFilename.Attribute.Set.elements t) in
     Lines.to_string lines
 
 end
@@ -744,7 +748,7 @@ module OPAM = struct
         @ listm   t.build_test    s_build_test    OpamFormat.make_command
         @ listm   t.build_doc     s_build_doc     OpamFormat.make_command
         @ option  t.depexts       s_depexts       OpamFormat.make_tags
-        @ List.map (fun (s, v) -> Variable (s, v)) t.others;
+        @ List.rev (List.rev_map (fun (s, v) -> Variable (s, v)) t.others);
     } in
     Syntax.to_string
       ~indent_variable:(fun s -> List.mem s [s_build ; s_remove ; s_depends ; s_depopts])
@@ -994,7 +998,7 @@ module Dot_config = struct
     ] in
     let parse_variables items =
       let l = List.filter (fun (x,_) -> not (List.mem x valid_fields)) (OpamFormat.variables items) in
-      List.map (fun (k,v) -> OpamVariable.of_string k, parse_value v) l in
+      List.rev_map (fun (k,v) -> OpamVariable.of_string k, parse_value v) l in
     let parse_requires = OpamFormat.parse_list (OpamFormat.parse_string |> OpamVariable.Section.of_string) in
     let parse_section kind s =
       let name =  OpamVariable.Section.of_string s.section_name in
@@ -1016,7 +1020,7 @@ module Dot_config = struct
       | B b -> Bool b
       | S s -> String s in
     let of_variables l =
-      List.map (fun (k,v) -> Variable (OpamVariable.to_string k, of_value v)) l in
+      List.rev_map (fun (k,v) -> Variable (OpamVariable.to_string k, of_value v)) l in
     let make_require = OpamVariable.Section.to_string |> OpamFormat.make_string in
     let of_section s =
       Section
@@ -1034,10 +1038,10 @@ module Dot_config = struct
       file_name     = OpamFilename.to_string filename;
       file_contents =
         of_variables t.variables
-        @ List.map of_section t.sections
+        @ List.rev (List.rev_map of_section t.sections)
     }
 
-  let variables t = List.map fst t.variables
+  let variables t = List.rev_map fst t.variables
 
   let variable t s = List.assoc s t.variables
 
@@ -1058,7 +1062,7 @@ module Dot_config = struct
     let find t name =
       List.find (fun s -> s.name = name) (M.get t)
 
-    let available t = List.map (fun s -> s.name) (M.get t)
+    let available t = List.rev_map (fun s -> s.name) (M.get t)
     let kind t s = (find t s).kind
     let bytecomp t s = (find t s).bytecomp
     let asmcomp  t s = (find t s).asmcomp
@@ -1066,7 +1070,7 @@ module Dot_config = struct
     let asmlink  t s = (find t s).asmlink
     let requires t s = (find t s).requires
     let variable t n s = List.assoc s (find t n).lvariables
-    let variables t n = List.map fst (find t n).lvariables
+    let variables t n = List.rev_map fst (find t n).lvariables
   end
 
   let filter t n = List.filter (fun s -> s.kind = n) t.sections
@@ -1252,7 +1256,7 @@ module Comp = struct
   let to_string filename s =
     let make_ppflag = function
       | Cmd l    -> OpamFormat.make_string_list l
-      | Camlp4 l -> List (Symbol "CAMLP4" :: List.map OpamFormat.make_string l) in
+      | Camlp4 l -> List (Symbol "CAMLP4" :: List.rev (List.rev_map OpamFormat.make_string l)) in
     Syntax.to_string {
       file_name     = OpamFilename.to_string filename;
       file_contents = [

src/core/opamFilename.ml
@@ -52,7 +52,7 @@ let mkdir dirname =
 
 let list_dirs d =
   let fs = OpamSystem.directories_with_links (Dir.to_string d) in
-  List.map Dir.of_string fs
+  List.rev (List.rev_map Dir.of_string fs)
 
 let in_dir dirname fn =
   if Sys.file_exists dirname then
@@ -63,7 +63,7 @@ let in_dir dirname fn =
 let exec dirname ?env ?name cmds =
   let env = match env with
     | None   -> None
-    | Some l -> Some (Array.of_list (List.map (fun (k,v) -> k^"="^v) l)) in
+    | Some l -> Some (Array.of_list (List.rev_map (fun (k,v) -> k^"="^v) l)) in
   in_dir dirname
     (fun () -> OpamSystem.commands ?env ?name cmds)
 
@@ -192,7 +192,7 @@ let chop_extension filename =
 
 let list_files d =
   let fs = OpamSystem.rec_files (Dir.to_string d) in
-  List.map of_string fs
+  List.rev (List.rev_map of_string fs)
 
 let copy ~src ~dst =
   OpamSystem.copy (to_string src) (to_string dst)
@@ -241,7 +241,8 @@ let download ~overwrite filename dirname =
 let download_iter ~overwrite filenames dirname =
   let rec aux = function
     | []   ->
-      OpamSystem.internal_error "Cannot download %s." (String.concat ", " (List.map to_string filenames))
+      let filenames = List.map to_string filenames in
+      OpamSystem.internal_error "Cannot download %s." (OpamMisc.pretty_list filenames)
     | h::t ->
       try download ~overwrite h dirname
       with _ -> aux t in

src/core/opamFormat.ml
@@ -80,7 +80,7 @@ let rec kind = function
   | Option(o,l) -> Printf.sprintf "option(%s,%s)" (kind o) (kinds l)
 
 and kinds l =
-  Printf.sprintf "{%s}" (String.concat " " (List.map kind l))
+  Printf.sprintf "{%s}" (String.concat " " (List.rev_map kind l))
 
 (* Base parsing functions *)
 let parse_bool = function
@@ -104,11 +104,11 @@ let parse_string = function
   | x        -> bad_format "Expecting a string, got %s" (kind x)
 
 let parse_list fn = function
-  | List s -> List.map fn s
+  | List s -> List.rev (List.rev_map fn s)
   | x      -> bad_format "Expecting a list, got %s" (kind x)
 
 let parse_group fn = function
-  | Group g -> List.map fn g
+  | Group g -> List.rev (List.rev_map fn g)
   | x        -> bad_format "Expecting a group, got %s" (kind x)
 
 let parse_option f g = function
@@ -177,11 +177,11 @@ let make_bool b = Bool b
 
 let make_int i = Int i
 
-let make_list fn l = List (List.map fn l)
+let make_list fn l = List (List.rev (List.rev_map fn l))
 
 let make_string_list = make_list make_string
 
-let make_group fn g = Group (List.map fn g)
+let make_group fn g = Group (List.rev (List.rev_map fn g))
 
 let make_option f g = function
   | (v, None)   -> f v
@@ -220,7 +220,7 @@ let rec pretty_string_of_value ?(indent_hint = []) = function
         (pretty_string_of_values ~indent_hint " " l)
 
 and pretty_string_of_values ?(indent_hint = []) sep l =
-  String.concat sep (List.map (pretty_string_of_value ~indent_hint) l)
+  String.concat sep (List.rev (List.rev_map (pretty_string_of_value ~indent_hint) l))
 
 let rec string_of_value = function
   | Symbol s
@@ -233,7 +233,7 @@ let rec string_of_value = function
   | Option(v,l) -> Printf.sprintf "%s {%s}" (string_of_value v) (string_of_values l)
 
 and string_of_values l =
-  String.concat " " (List.map string_of_value l)
+  String.concat " " (List.rev (List.rev_map string_of_value l))
 
 let incr tab = "  " ^ tab
 
@@ -262,7 +262,7 @@ let assoc items n parse =
   with Not_found -> bad_format "Field %S is missing" n
 
 let get_all_section_by_kind items kind =
-  try List.map snd (List.find_all (fun (k,_) -> k=kind) (sections items))
+  try List.rev_map snd (List.find_all (fun (k,_) -> k=kind) (sections items))
   with Not_found -> bad_format "Section kind %S is missing" kind
 
 let get_section_by_kind items kind =
@@ -270,7 +270,7 @@ let get_section_by_kind items kind =
   with Not_found -> bad_format "Section kind %S is missing" kind
 
 let assoc_sections items kind parse =
-  List.map parse (get_all_section_by_kind items kind)
+  List.rev_map parse (get_all_section_by_kind items kind)
 
 let assoc_option items n parse =
   try Some (parse (List.assoc n (variables items)))

src/core/opamFormula.ml
@@ -45,26 +45,26 @@ let string_of_atom = function
 type 'a conjunction = 'a list
 
 let string_of_conjunction string_of_atom c =
-  Printf.sprintf "(%s)" (String.concat " & " (List.map string_of_atom c))
+  Printf.sprintf "(%s)" (String.concat " & " (List.rev_map string_of_atom c))
 
 type 'a disjunction = 'a list
 
 let string_of_disjunction string_of_atom c =
-  Printf.sprintf "(%s)" (String.concat " | " (List.map string_of_atom c))
+  Printf.sprintf "(%s)" (String.concat " | " (List.rev_map string_of_atom c))
 
 type 'a cnf = 'a list list
 
 let string_of_cnf string_of_atom cnf =
   let string_of_clause c =
-    Printf.sprintf "(%s)" (String.concat " | " (List.map string_of_atom c)) in
-  Printf.sprintf "(%s)" (String.concat " & " (List.map string_of_clause cnf))
+    Printf.sprintf "(%s)" (String.concat " | " (List.rev_map string_of_atom c)) in
+  Printf.sprintf "(%s)" (String.concat " & " (List.rev_map string_of_clause cnf))
 
 type 'a dnf = 'a list list
 
 let string_of_dnf string_of_atom cnf =
   let string_of_clause c =
-    Printf.sprintf "(%s)" (String.concat " & " (List.map string_of_atom c)) in
-  Printf.sprintf "(%s)" (String.concat " | " (List.map string_of_clause cnf))
+    Printf.sprintf "(%s)" (String.concat " & " (List.rev_map string_of_atom c)) in
+  Printf.sprintf "(%s)" (String.concat " | " (List.rev_map string_of_clause cnf))
 
 type 'a formula =
   | Empty
@@ -223,7 +223,7 @@ let ands = function
   | h::t -> List.fold_left (fun acc elt -> And(acc, elt)) h t
 
 let of_conjunction c =
-  of_atom_formula (ands (List.map (fun x -> Atom x) c))
+  of_atom_formula (ands (List.rev_map (fun x -> Atom x) c))
 
 let to_disjunction t =
   match to_cnf t with
@@ -236,7 +236,7 @@ let ors = function
   | h::t -> List.fold_left (fun acc elt -> Or(acc, elt)) h t
 
 let of_disjunction d =
-  of_atom_formula (ors (List.map (fun x -> Atom x) d))
+  of_atom_formula (ors (List.rev_map (fun x -> Atom x) d))
 
 let atoms t =
   fold_left (fun accu x -> x::accu) [] (to_atom_formula t)

src/core/opamMisc.ml
@@ -44,7 +44,18 @@ end
 
 let string_of_list f = function
   | [] -> "{}"
-  | l  -> Printf.sprintf "{ %s }" (String.concat ", " (List.map f l))
+  | l  ->
+    let buf = Buffer.create 1024 in
+    let n = List.length l in
+    let i = ref 0 in
+    Buffer.add_string buf "{ ";
+    List.iter (fun x ->
+      incr i;
+      Buffer.add_string buf (f x);
+      if !i <> n then Buffer.add_string buf ", ";
+    ) l;
+    Buffer.add_string buf " }";
+    Buffer.contents buf
 
 let rec pretty_list = function
   | []    -> ""
@@ -70,11 +81,11 @@ module Set = struct
       List.fold_left (fun set e -> add e set) empty l
 
     let to_string s =
-      let l = fold (fun nv l -> O.to_string nv :: l) s [] in
-      string_of_list (fun x -> x) l
+      let l = S.fold (fun nv l -> O.to_string nv :: l) s [] in
+      string_of_list (fun x -> x) (List.rev l)
 
     let map f t =
-      of_list (List.map f (elements t))
+      S.fold (fun e set -> S.add (f e) set) t S.empty
 
     let find fn s =
       choose (filter fn s)
@@ -91,9 +102,11 @@ module Map = struct
 
     include M
 
-    let values map = List.map snd (bindings map)
+    let values map =
+      List.rev (M.fold (fun _ v acc -> v :: acc) map [])
 
-    let keys map = List.map fst (bindings map)
+    let keys map =
+      List.rev (M.fold (fun k _ acc -> k :: acc) map [])
 
     let union f m1 m2 =
       M.fold (fun k v m ->
@@ -259,7 +272,7 @@ let insert comp x l =
 
 let env = lazy (
   let e = Unix.environment () in
-  List.map (fun s ->
+  List.rev_map (fun s ->
     match cut_at s '=' with
     | None   -> s, ""
     | Some p -> p

commit 9623e905734660c4fd92efb033727917122757cc

Fix embarassing #551
This is a regression introduced in a7ec825f8dbc3c7a3fcb7a91617c0d623105125e (pre 1.0.0)

src/client/opamClient.ml
@@ -940,7 +940,7 @@ module API = struct
     let depends =
       let universe = OpamState.universe t Depends in
       OpamSolver.reverse_dependencies ~depopts:true ~installed:true universe reinstall in
-    let to_process = List.rev_map (fun pkg -> To_recompile pkg) depends in
+    let to_process = List.map (fun pkg -> To_recompile pkg) depends in
     let solution = OpamSolution.apply t Reinstall (OpamSolver.sequential_solution to_process) in
     OpamSolution.check_solution solution
 