3c009cb11d7c0475847994f49a58a30e4c09faba
Thomas Gazagnaire <thomas@gazagnaire.org>
2013-07-17 14:34:24 +0200

    Tentative fix for #680
    I've tested it against the ocsigen repo and it seems to work. Reviews for Darcs experts are welcome.

--- src/repositories/opamDarcs.ml
+++ src/repositories/opamDarcs.ml
@@ -27,66 +27,35 @@ module Darcs = struct
   let init repo =
     let repo = OpamFilename.Dir.to_string repo.repo_address in
     OpamSystem.commands [
-      (* Initialize a new darcs repository, and set a default source
-         repository.  The dummy tag prevents the patches from being
-         actually fetched. We use the 'fetch' command because there is
-         no dedicated command with darcs to set a remote repository, as
-         'git remote'. *)
-      [ "darcs" ; "initialize" ];
-      [ "darcs" ; "fetch" ; "--tags=ThisIsADummyTag#00"; "--set-default" ; repo ];
+      [ "darcs" ; "init" ];
+      [ "darcs" ; "get" ; repo; "--lazy" ];
     ]
 
-  let fetch repo =
-    OpamFilename.in_dir repo.repo_root (fun () ->
-      (* Fetch the changes and save them to a temporary patch bundle *)
-      OpamSystem.command [ "darcs" ; "fetch"; "--all"; "--output=opam_update.bundle"]
-    )
+  (* With darcs, it is apparently easier to compute a diff between
+     remote and local, without fething at all. So we set fetch to be a
+     no-op. *)
+  let fetch _ =
+    ()
 
+  (* Merge is actually a full pull *)
   let merge repo =
     OpamFilename.in_dir repo.repo_root (fun () ->
-      let patches_bundle = OpamFilename.of_string "opam_update.bundle" in
-      if OpamFilename.exists patches_bundle then
-        OpamSystem.command [ "darcs" ; "apply"; "opam_update.bundle" ];
-      OpamFilename.remove patches_bundle
+        let repo = OpamFilename.Dir.to_string repo.repo_address in
+        OpamSystem.command [
+          "darcs" ; "pull"; repo; "--all"; "--quiet"
+        ];
     )
 
-  (* Look for file pathes {packages,compilers}/* in a set of XML lines. *)
-  let files_of_xmlchanges lines =
-    let rex = Re_perl.compile_pat "((packages|compilers)((/[\\.\\w]+))+)" in
-    let rec aux acc = function
-      | [] -> acc
-      | h :: t ->
-        try
-          let subs = Re.exec rex h in
-          let file = Re.get subs 1 in
-          aux (file :: acc) t
-        with Not_found ->
-          aux acc t
-    in
-    aux [] lines
-
-  (* Return the list of modified files of the darcs repository located
-     at [dirname].  There is no simple way to get a diff of files with
-     differences between the local and a remote repository, with darcs,
-     as 'git diff --name-only'. We use the following workaround: 1. Tag
-     the current state of the repo 'opam_update' 2. Pull (fetch and
-     apply) all new patches 3. Get the changes made to the repo since
-     the 'opam_update' tag, in a XML format 4. Back to initial state:
-     obliterate the 'opam_update' tag and all subsequent patches *)
+  (* Difference between remote and local is a 'pull --dry-run' *)
   let diff repo =
     OpamFilename.in_dir repo.repo_root (fun () ->
-      OpamSystem.commands [
-        [ "darcs" ; "tag" ; "--author=opam@ocamlpro.com" ; "opam_update" ] ;
-        [ "darcs" ; "pull"; "--all" ]
-      ];
-      let xml_changes = OpamSystem.read_command_output
-          [ "darcs" ; "changes" ; "--xml-output" ; "--summary" ;
-            "--from-tag=opam_update" ] in
-      let files = files_of_xmlchanges xml_changes in
-      OpamSystem.command
-        [ "darcs" ; "obliterate" ; "--all" ; "--from-tag=opam_update" ];
-      files <> []
-    )
+        let repo = OpamFilename.Dir.to_string repo.repo_address in
+        let patches =
+          OpamSystem.read_command_output [
+            "darcs" ; "pull" ; repo; "--dry-run" ; "--quiet"
+          ] in
+        patches <> []
+      )
 
   let revision _ =
     "<darcs-???>"
