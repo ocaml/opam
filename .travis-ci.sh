b970465224a1ecf01ba860d7d9dc3ddc95b0bd52
Anil Madhavapeddy <anil@recoil.org>
2013-09-22 17:51:10 -0400

    add more tests, but redirect output due to Travis output limit (4MB)

--- .travis-ci.sh
+++ .travis-ci.sh
@@ -1,11 +1,13 @@
 sudo apt-get install -qq ocaml
 ./configure
 make
+make tests > tests-3121.log 2>&1
 make distclean
 
 sudo apt-get install -qq ocaml-native-compilers
 ./configure
 make
+make tests > tests-3121native.log 2>&1
 make distclean
 
 echo "yes" | sudo add-apt-repository ppa:avsm/ppa-testing
@@ -18,4 +20,4 @@ sudo make install
 export OPAMYES=1
 opam init
 opam install lwt
-make tests
+make tests > tests.log 2>&1
