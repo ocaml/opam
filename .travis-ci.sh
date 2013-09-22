6da5cfe025646e89b1baf3caeb89ef40960dd801
Anil Madhavapeddy <anil@recoil.org>
2013-09-22 14:41:48 -0400

    travis: add configure steps for Travis

--- .travis-ci.sh
+++ .travis-ci.sh
@@ -1,9 +1,11 @@
 sudo apt-get install -qq ocaml
+./configure
 make
 make tests
 make distclean
 
 sudo apt-get install -qq ocaml-native-compilers
+./configure
 make
 make tests
 make distclean
@@ -11,5 +13,6 @@ make distclean
 echo "yes" | sudo add-apt-repository ppa:avsm/ppa-testing
 sudo apt-get update -qq
 sudo apt-get -y upgrade
+./configure
 make
 make tests
