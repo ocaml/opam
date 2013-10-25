74515600fc2809e8f79402e6d3b1fe30b1e31d02
Louis Gesbert <louis.gesbert@ocamlpro.com>
2013-10-25 12:41:50 +0200

    travis: insist on apt-get upgrade being non interactive

--- .travis-ci.sh
+++ .travis-ci.sh
@@ -1,4 +1,4 @@
-sudo apt-get install -qq ocaml
+sudo apt-get install -y -qq ocaml
 git config --global user.email "travis@example.com"
 git config --global user.name "Travis CI"
 ./configure
@@ -6,15 +6,15 @@ make
 make tests > tests-3121.log 2>&1 || (tail -1000 tests-3121.log && exit 1)
 make distclean
 
-sudo apt-get install -qq ocaml-native-compilers
+sudo apt-get install -y -qq ocaml-native-compilers
 ./configure
 make
 make tests > tests-3121native.log 2>&1
 make distclean
 
-echo "yes" | sudo add-apt-repository ppa:avsm/ppa-testing
+sudo add-apt-repository -y ppa:avsm/ppa-testing
 sudo apt-get update -qq
-sudo apt-get -y upgrade
+sudo apt-get -y --force-yes upgrade
 ./configure
 make
 sudo make install
