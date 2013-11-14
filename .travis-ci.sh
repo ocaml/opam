a94413e7780f0cd546231884a33c4a23aa74efd9
Thomas Gazagnaire <thomas@gazagnaire.org>
2013-11-14 10:49:32 +0000

    [travis] use the system libraries to compile opam-rt

--- .travis-ci.sh
+++ .travis-ci.sh
@@ -19,21 +19,25 @@ echo OCaml version
 ocaml -version
 
 # Compile OPAM and run the basic tests
-export PATH=$HOME/bin:$PATH
-mkdir -p $HOME/bin
-./configure --prefix=$HOME
+./configure
 make
 make tests > tests.log 2>&1 || (tail -1000 tests.log && exit 1)
-make install
 
+# Let's see if basic install works
+sudo make install
 export OPAMYES=1
 opam init
 opam install lwt
 
-# Run opam-rt
+# Recompile OPAM using the system libraries (installed using OPAM),
+# install the latest OPAM-lib and run opam-rt
 if [ "$OPAM_TEST" = "1" ]; then
-    eval `opam config env` # put ocamlfind in the PATH
+    eval `opam config env`
     opam install lwt cohttp ssl cmdliner ocamlgraph dose cudf re
+    make distclean
+    ./configure
+    make prepare
+    make compile
     make libinstall
     wget https://github.com/ocaml/opam-rt/archive/master.tar.gz
     tar xvfz master.tar.gz
