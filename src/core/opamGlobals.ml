e3883f2e2a9d9471ff3a5da24c9bb09675815c05
Roberto Di Cosmo <github@dicosmo.org>
2014-02-21 02:25:23 +0100

    Proper quoting of user preferences possibly containing ( and ) now moved to dose patch; added help entry for OPAMCRITERIA with default value printed out.

--- src/core/opamGlobals.ml
+++ src/core/opamGlobals.ml
@@ -77,11 +77,11 @@ let solver_timeout =
   try float_of_string (OpamMisc.getenv "OPAMSOLVERTIMEOUT")
   with Not_found | Failure _ -> 5.
 
-(* we quote ( and ) to ensure they pass through the shell intact; 
-   this armour phase should be implemented in Dose eventually *)
-let aspcud_criteria =
+let default_preferences = "-removed,-notuptodate,-count(down),-new,-changed"
+
+let solver_preferences =
   try OpamMisc.strip (OpamMisc.getenv "OPAMCRITERIA")
-  with Not_found -> "-removed,-notuptodate,-count\\(down\\),-new,-changed"
+  with Not_found -> default_preferences
 
 let default_repository_name    = "default"
 let default_repository_address = "https://opam.ocaml.org"
