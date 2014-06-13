b30f247bd2c823fe69a340453e195920c546dba6
Louis Gesbert <louis.gesbert@ocamlpro.com>
2014-06-13 18:16:42 +0200

    Extending interface of opam-admin.top
    It's sometimes useful to get access to all its files when rewriting inside a package

--- src/scripts/opam_admin_top.ml
+++ src/scripts/opam_admin_top.ml
@@ -22,62 +22,114 @@ let repo = OpamRepository.local (OpamFilename.cwd ())
 let packages = OpamRepository.packages repo
 let compilers = OpamRepository.compilers repo 
 
-let map_packages ?(opam=identity) ?(descr=identity) ?(url=identity) ?(dot_install=identity) () =
+let wopt w f = function
+  | None -> OpamFilename.remove f
+  | Some contents -> w f contents
 
+let map_packages_gen f =
   let packages = OpamRepository.packages_with_prefixes repo in
+  let changed_pkgs = ref 0 in
+  let changed_files = ref 0 in
 
   (** packages *)
   OpamPackage.Map.iter (fun package prefix ->
-      OpamGlobals.msg "Processing package %s\n" (OpamPackage.to_string package);
+      OpamGlobals.msg "Processing package %s... " (OpamPackage.to_string package);
 
       let opam_file = OpamPath.Repository.opam repo prefix package in
-      let opam1 = OpamFile.OPAM.read opam_file in
-      let opam2 = opam opam1 in
-      if opam1 <> opam2 then OpamFile.OPAM.write opam_file opam2;
+      let opam = OpamFile.OPAM.read opam_file in
 
       let descr_file = OpamPath.Repository.descr repo prefix package in
-      if OpamFilename.exists descr_file then (
-        let descr1 = OpamFile.Descr.read descr_file in
-        let descr2 = descr descr1 in
-        if descr1 <> descr2 then OpamFile.Descr.write descr_file descr2
-      );
+      let descr =
+        if OpamFilename.exists descr_file then
+          Some (OpamFile.Descr.read descr_file)
+        else None
+      in
 
       let url_file = OpamPath.Repository.url repo prefix package in
-      if OpamFilename.exists url_file then (
-        let url1 = OpamFile.URL.read url_file in
-        let url2 = url url1 in
-        if url1 <> url2 then OpamFile.URL.write url_file url2
-      );
+      let url =
+        if OpamFilename.exists url_file then
+          Some (OpamFile.URL.read url_file)
+        else None
+      in
 
       let dot_install_file =
         OpamPath.Repository.files repo prefix package
         // (OpamPackage.Name.to_string (OpamPackage.name package) ^ ".install") in
-      if OpamFilename.exists dot_install_file then (
-        let dot_install1 = OpamFile.Dot_install.read dot_install_file in
-        let dot_install2 = dot_install dot_install1 in
-        if dot_install1 <> dot_install2 then
-          OpamFile.Dot_install.write dot_install_file dot_install2
-      );
-    ) packages;
-  OpamGlobals.msg "Done.\n"
+      let dot_install =
+        if OpamFilename.exists dot_install_file then
+          Some (OpamFile.Dot_install.read dot_install_file)
+        else None
+      in
 
-let map_compilers ?(comp=identity) ?(descr=identity) () =
-  (** compilers *)
+      let opam2, descr2, url2, dot_install2 =
+        f package ~opam ~descr ~url ~dot_install
+      in
+
+      let changed = ref false in
+      let upd () = changed := true; incr changed_files in
+      if opam <> opam2 then
+        (upd (); OpamFile.OPAM.write opam_file opam2);
+      if descr <> descr2 then
+        (upd (); wopt OpamFile.Descr.write descr_file descr2);
+      if url <> url2 then
+        (upd (); wopt OpamFile.URL.write url_file url2);
+      if dot_install <> dot_install2 then
+        (upd (); wopt OpamFile.Dot_install.write dot_install_file dot_install2);
+
+      if !changed then
+        (incr changed_pkgs;
+         OpamGlobals.msg "\r\027[KUpdated %s\n" (OpamPackage.to_string package))
+      else
+        OpamGlobals.msg "\r\027[K";
+    ) packages;
+  OpamGlobals.msg "Done. Updated %d files in %d packages.\n"
+    !changed_files !changed_pkgs
+
+let map_packages ?(opam=identity) ?(descr=identity) ?(url=identity) ?(dot_install=identity) () =
+  let omap = OpamMisc.Option.map in
+  map_packages_gen (fun _ ~opam:o ~descr:d ~url:u ~dot_install:i ->
+      opam o, omap descr d, omap url u, omap dot_install i)
+
+let map_compilers_gen f =
   let compilers = OpamRepository.compilers_with_prefixes repo in
 
+  let changed_comps = ref 0 in
+  let changed_files = ref 0 in
+
   OpamCompiler.Map.iter (fun c prefix ->
-      OpamGlobals.msg "Processing compiler %s\n" (OpamCompiler.to_string c);
+      OpamGlobals.msg "Processing compiler %s... " (OpamCompiler.to_string c);
 
       let comp_file = OpamPath.Repository.compiler_comp repo prefix c in
-      let comp1 = OpamFile.Comp.read comp_file in
-      let comp2 = comp comp1 in
-      if comp1 <> comp2 then OpamFile.Comp.write comp_file comp2;
+      let comp = OpamFile.Comp.read comp_file in
 
       let descr_file = OpamPath.Repository.compiler_descr repo prefix c in
-      if OpamFilename.exists descr_file then (
-        let descr1 = OpamFile.Descr.read descr_file in
-        let descr2 = descr descr1 in
-        if descr2 <> descr1 then OpamFile.Descr.write descr_file descr2
-      );
+      let descr =
+        if OpamFilename.exists descr_file then
+          Some (OpamFile.Descr.read descr_file)
+        else None
+      in
+
+      let comp2, descr2 =
+        f c ~comp ~descr
+      in
+
+      let changed = ref false in
+      let upd () = changed := true; incr changed_files in
+
+      if comp <> comp2 then
+        (upd (); OpamFile.Comp.write comp_file comp2);
+      if descr <> descr2 then
+        (upd (); wopt OpamFile.Descr.write descr_file descr2);
+
+      if !changed then
+        (incr changed_comps;
+         OpamGlobals.msg "\r\027[KUpdated %s\n" (OpamCompiler.to_string c))
+      else
+        OpamGlobals.msg "\r\027[K";
     ) compilers;
-  OpamGlobals.msg "Done.\n"
+  OpamGlobals.msg "Done. Updated %d files in %d compiler descriptions.\n"
+    !changed_files !changed_comps
+
+let map_compilers ?(comp=identity) ?(descr=identity) () =
+  map_compilers_gen (fun _ ~comp:c ~descr:d ->
+      comp c, OpamMisc.Option.map descr d)
