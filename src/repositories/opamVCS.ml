531e414052e886f8e4fcec4b7e5bcb96dafebe46
Louis Gesbert <louis.gesbert@ocamlpro.com>
2014-12-09 11:44:41 +0900

    Remove debug

--- src/repositories/opamVCS.ml
+++ src/repositories/opamVCS.ml
@@ -49,7 +49,6 @@ module Make (VCS: VCS) = struct
     in
     VCS.versionned_files source_repo
     @@+ fun files ->
-    List.iter prerr_endline files;
     let stdout_file =
       let f = OpamSystem.temp_file "rsync-files" in
       let fd = open_out f in
