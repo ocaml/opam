ocaml/opam

commit fef5f0704f59459dad119dbb30f63e92babc6d1f

Back out the optimisation to recompile only when archive checksum changs
The observable bug is that when I update the `url` file in a package
to change the `archive` source but not the checksum, this never
propagates after an `opam update`.  The reason is that the update
is skipped due to the optimisation that prevents recompilation of a
package when the location changes, but not the checksum.

Unfortunately, if using OPAM with a `local` remote, this makes it
impossible to fix a broken location without manually munging around
in `~/.opam`.

Debugged with @samoht

src/core/opamRepository.ml
@@ -267,12 +267,7 @@ let package_state repo prefix nv all =
   let fs = match all with
     | `all       -> package_files repo prefix nv ~archive:true
     | `partial b -> package_important_files repo prefix nv ~archive:b in
-  let url = OpamPath.Repository.url repo prefix nv in
-  let l =
-    List.map (fun f ->
-        if f = url then url_checksum f
-        else OpamFilename.checksum f)
-      fs in
+  let l = List.map OpamFilename.checksum fs in
   List.flatten l
 
 (* Sort repositories by priority *)

commit 4495dd360b596a369836cd76ac14a089a16ee791

Fix update bug introduced in fef5f0704f59459dad119dbb30f63e92babc6d1f
OPAM would reinstall all packages after each update :(
The 'package states' are not well defined, and the interface between OpamRepository and OpamState
for that purpose is very fragile (it relies in the same lists being computed, in the same order,
from two modules). But since this will go in 1.3, there is no point in rewriting it now.

src/client/opamState.ml
@@ -183,7 +183,7 @@ let package_state_one t all nv =
     | `all ->
       OpamFilename.checksum opam
       @ OpamFilename.checksum descr
-      @ OpamRepository.url_checksum url
+      @ OpamFilename.checksum url
       @ OpamFilename.checksum_dir files
       @ OpamFilename.checksum archive
     | `partial true ->

src/core/opamRepository.ml
@@ -267,7 +267,12 @@ let package_state repo prefix nv all =
   let fs = match all with
     | `all       -> package_files repo prefix nv ~archive:true
     | `partial b -> package_important_files repo prefix nv ~archive:b in
-  let l = List.map OpamFilename.checksum fs in
+  let url = OpamPath.Repository.url repo prefix nv in
+  let l =
+    List.map (fun f ->
+        if all <> `all && f = url then url_checksum f
+        else OpamFilename.checksum f)
+      fs in
   List.flatten l
 
 (* Sort repositories by priority *)

src/core/opamRepository.mli
@@ -62,15 +62,19 @@ val compiler_index: repository repository_name_map -> (repository_name * string 
 
 (** {2 State} *)
 
-(** Get the meaningful checksum off an url file *)
+(** Get the package archive checksum off an url file *)
 val url_checksum: OpamFilename.t -> checksums
 
 (** Get all the package files *)
 val package_files: repository -> string option -> package -> archive:bool ->
   filename list
 
-(** Compute a package state (ie. a list of checksums). *)
-val package_state: repository -> string option -> package -> [`all|`partial of bool]
+(** Compute a package state (ie. a list of checksums). If [`partial archive],
+    only the checksum of the archive within the url file (instead of the file
+    itself), of the files/ subdirectory, and of the archive if set are
+    returned. *)
+val package_state: repository -> string option -> package ->
+  [`all|`partial of bool]
   -> checksums
 
 (** Get all the compiler files *)