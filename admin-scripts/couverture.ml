8864528212d228581e079a908b4e0f15cdd5f3a4
Louis Gesbert <louis.gesbert@ocamlpro.com>
2015-06-23 14:49:10 +0900

    Add a script providing a couverture for all packages
    Given a set of packages (or by default, all), admin-scripts/couverture.ml will return sets of coinstallable packages that cover the set.
    
    There is no warranty that it is done in a minimal number of steps, but all installable packages will be covered.
    An external solver is absolutely required.
    
    The approach is very interesting and could be used in opam proper for improving several cases:
    - switch import, to have all (root) packages as _wished for_, but not mandatory; a first criterion could maximise the number of wished packages at the right version, a second the number of wished packages on a different version.
    - on failure, we could store previously installed roots and have a command to attempt to restore them without resorting to importing the exact backed up state. 'installed_roots' could actually be just 'roots'. That would be much better, but we need to be careful designing the interface, so that it doesn't confuse people.
    
    it got better, but it is still annoying when an upgrade fails and we lost installed packages that we wanted, having later to explicitely ask opam to reinstall them...
    
    NOTE: although it's a script, it's pretty slow unless compiled to native. Use `make -C admin-scripts couverture`.

---
+++ admin-scripts/couverture.ml
@@ -0,0 +1,129 @@
+#!/usr/bin/env opam-admin.top
+
+#directory "+../opam-lib";;
+
+(**************************************************************************)
+(*                                                                        *)
+(*    Copyright 2015 OCamlPro                                             *)
+(*                                                                        *)
+(*  All rights reserved.This file is distributed under the terms of the   *)
+(*  GNU Lesser General Public License version 3.0 with linking            *)
+(*  exception.                                                            *)
+(*                                                                        *)
+(*  OPAM is distributed in the hope that it will be useful, but WITHOUT   *)
+(*  ANY WARRANTY; without even the implied warranty of MERCHANTABILITY    *)
+(*  or FITNESS FOR A PARTICULAR PURPOSE.See the GNU General Public        *)
+(*  License for more details.                                             *)
+(*                                                                        *)
+(**************************************************************************)
+
+(** This script gives scenarios to install all named packages in a given set.
+    This may require several steps, in case of conflicts.
+
+    Consistent installation steps are printed one per line to stdout. Stderr
+    gives more detail.
+
+    Relies on the current opam root for the list of available packages, i.e.
+    depends on configured remotes, OS and OCaml version, but not on the set of
+    currently installed packages.
+*)
+
+open OpamTypes
+
+let max_install t inst_packages =
+  let universe = OpamState.universe t Depends in
+  let wish_field = "wished" in
+  let universe =
+    { universe with u_installed = OpamPackage.Set.empty;
+                    u_installed_roots = OpamPackage.Set.empty;
+                    u_attrs = [wish_field, inst_packages]; }
+  in
+  if not (OpamCudf.external_solver_available ()) then
+    failwith "No external solver found";
+  let preferences =
+    let preferences = OpamSolverConfig.criteria `Default in
+    Some (lazy (Printf.sprintf "+sum(solution,%s),%s" wish_field preferences))
+  in
+  OpamSolverConfig.update ~solver_preferences_default:preferences ();
+  let version_map =
+    OpamSolver.cudf_versions_map universe universe.u_available
+  in
+  let request = {
+    wish_install = [];
+    wish_remove = [];
+    wish_upgrade = [];
+    extra_attributes = [wish_field];
+    criteria = `Default;
+  } in
+  let cudf_universe =
+    OpamSolver.load_cudf_universe ~build:true universe ~version_map
+      universe.u_available
+  in
+  match OpamCudf.resolve ~extern:true ~version_map cudf_universe request with
+  | Conflicts _ -> failwith "Solver error (unexpected conflicts)"
+  | Success u ->
+    OpamPackage.Set.of_list (List.map OpamCudf.cudf2opam (OpamCudf.packages u))
+
+let rec couverture acc t pkgs =
+  Printf.eprintf "# -> Step %d, %d packages remaining.\n%!"
+    (List.length acc + 1)
+    (OpamPackage.(Name.Set.cardinal (names_of_packages pkgs)));
+  let step = max_install t pkgs in
+  if OpamPackage.Set.is_empty step then
+    List.rev acc, pkgs
+  else
+  let pkgs =
+    OpamPackage.Set.filter
+      (fun nv -> not (OpamPackage.has_name step (OpamPackage.name nv))) pkgs
+  in
+  couverture (step::acc) t pkgs
+
+module P = OpamPackage
+open P.Set.Op
+
+let () =
+  let root = OpamStateConfig.opamroot () in
+  OpamFormatConfig.init ();
+  if not (OpamStateConfig.load_defaults root) then
+    failwith "Opam root not found";
+  OpamStd.Config.init ();
+  OpamSolverConfig.init ();
+  OpamStateConfig.init ();
+  let t =
+    OpamState.load_state ~save_cache:false "couverture"
+      OpamStateConfig.(!r.current_switch)
+  in
+  let avail = Lazy.force t.OpamState.Types.available_packages in
+  let wanted = match Array.to_list Sys.argv with
+    | [] | _::[] -> avail
+    | _::l ->
+      List.fold_left (fun wanted name ->
+          let nvs =
+            if String.contains name '.' then
+              P.Set.singleton (P.of_string name)
+            else
+              P.packages_of_name avail
+                (P.Name.of_string name)
+          in
+          if P.Set.is_empty (nvs %% avail) then
+            failwith (Printf.sprintf "Package %s not found" name)
+          else
+            wanted ++ nvs
+        ) P.Set.empty l
+  in
+  let couv,remaining = couverture [] t wanted in
+  let avail_names = P.names_of_packages avail in
+  let remaining_names = P.names_of_packages remaining in
+  Printf.eprintf "# Found a couverture for %d over %d packages in %d steps:\n%!"
+    (P.Name.Set.cardinal (P.Name.Set.diff avail_names remaining_names))
+    (P.Name.Set.cardinal avail_names)
+    (List.length couv);
+  List.iter (fun s ->
+      print_endline
+        (OpamStd.List.concat_map " " OpamPackage.to_string
+           (P.Set.elements s)))
+    couv;
+  Printf.eprintf "# %d uninstallable packages remain: %s\n%!"
+    (P.Name.Set.cardinal remaining_names)
+    (OpamStd.List.concat_map " " OpamPackage.Name.to_string
+       (P.Name.Set.elements remaining_names))
