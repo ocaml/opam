2ad3becb6c4d52fb43c209498426fa28d4a998f0
Louis Gesbert <louis.gesbert@ocamlpro.com>
2015-07-29 16:53:18 +0900

    Getting rid of Re_str

--- src/core/opamStd.ml
+++ src/core/opamStd.ml
@@ -404,10 +404,11 @@ module OpamString = struct
   let rcut_at = cut_at_aux String.rindex
 
   let split s c =
-    Re_str.split (Re_str.regexp (Printf.sprintf "[%c]+" c)) s
+    Re.(split (compile (rep1 (char c))) s)
 
   let split_delim s c =
-    Re_str.split_delim (Re_str.regexp (Printf.sprintf "[%c]" c)) s
+    let s = if String.length s <> 0 then Printf.sprintf "%c%s%c" c s c else s in
+    Re.(split (compile (rep1 (char c))) s)
 
   let fold_left f acc s =
     let acc = ref acc in
