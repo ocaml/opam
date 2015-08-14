27d6d2ad1634e9a9dee407c0bac9476ff7ca4df1
Amir Chaudhry <amirmc@gmail.com>
2015-08-14 15:03:47 +0100

    update Travis scripts

--- .travis-ci.sh
+++ .travis-ci.sh
@@ -8,7 +8,7 @@ install_on_linux () {
   3.12.1) ppa=avsm/ocaml312+opam12 ;;
   4.00.1) ppa=avsm/ocaml40+opam12 ;;
   4.01.0) ppa=avsm/ocaml41+opam12 ;;
-  4.02.1) ppa=avsm/ocaml42+opam12 ;;
+  4.02.3) ppa=avsm/ocaml42+opam12 ;;
   *) echo Unknown $OCAML_VERSION; exit 1 ;;
   esac
 
