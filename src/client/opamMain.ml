80d0174fbdd5304a235f2b79d17e39f0ff67c54c
Louis Gesbert <louis.gesbert@ocamlpro.com>
2016-04-15 16:08:17 +0900

    Disallow plugins with name prefixes of opam command
    It's very common in cmdliner to abbreviate commands; in this case, opam
    would always load the full state to find a corresponding plugin package,
    which incurred a delay (0.23s got down to 0.06s for 'opam swi', on a
    SSD).
    
    This also sounds like a reasonable sanity check.

--- src/client/opamMain.ml
+++ src/client/opamMain.ml
@@ -1589,7 +1589,9 @@ let check_and_run_external_commands () =
   | _ :: name :: args ->
     if
       not (OpamStd.String.starts_with ~prefix:"-" name)
-      && List.for_all (fun (_,info) -> Term.name info <> name) commands
+      && List.for_all (fun (_,info) ->
+          not (OpamStd.String.starts_with ~prefix:name (Term.name info)))
+        commands
     then
       (* No such command, check if there is a matching plugin *)
       let command = plugin_prefix ^ name in
