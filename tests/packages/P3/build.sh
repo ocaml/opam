e5cb7d000eb813ec38b2b6e18d25c92667ed1696
David Allsopp <david.allsopp@metastack.com>
2017-09-01 20:38:35 +0100

    Testsuite fixes for Windows
    Allow for:
    
     - Spaces in PATH
     - Native tools returning \r
     - Converting paths between Cygwin and Native
    
    Signed-off-by: David Allsopp <david.allsopp@metastack.com>

--- tests/packages/P3/build.sh
+++ tests/packages/P3/build.sh
@@ -3,7 +3,7 @@
 echo "Building P3 version ${OPAM_PACKAGE_VERSION}"
 
 if [ "x${OPAM_PACKAGE_NAME}" = "xP3" ]; then
-    LIB=$(${OPAM} config var lib)
+    LIB=$(${OPAM} config var lib | tr -d '\r')
     ocamlc -a -I $LIB/P1 -I $LIB/P2 p3.ml -o p3.cma
     ocamlopt -a -I $LIB/P1 -I $LIB/P2 p3.ml -o p3.cmxa
     ocamlc -a -I $LIB/P1 -I $LIB/P2 p3_bar.ml -o p3_bar.cma
