59cc3916e5c055960c5ecf35eb30e9b1b135b078
Louis Gesbert <louis.gesbert@ocamlpro.com>
2017-09-06 14:55:10 +0200

    Fix Travis (force lwt>=3.1.0)
    On OSX, we use opam 1.2.2 without aspcud to bootstrap, and an older
    version gets chosen. `opam-rt` actually requires the newer one.

--- .travis-ci.sh
+++ .travis-ci.sh
@@ -15,7 +15,7 @@ install-bootstrap () {
         # TEMPORARY: waiting for merge into opam-repository
         ( cd ${OPAMBSROOT} && git clone https://github.com/AltGr/ocaml-mccs && cd ocaml-mccs && git checkout 1.1+2b && opam pin add mccs.1.1+2 . --yes; )
 
-        opam install ocamlfind ocamlbuild cohttp cohttp-lwt-unix ssl cmdliner dose3 opam-file-format re jbuilder.1.0+beta12 mccs --yes
+        opam install ocamlfind ocamlbuild cohttp cohttp-lwt-unix 'lwt>=3.1.0' ssl cmdliner dose3 opam-file-format re jbuilder.1.0+beta12 mccs --yes
         # Allow use of ocamlfind packages in ~/local/lib
         FINDCONF=$(ocamlfind printconf conf)
         sed "s%^path=.*%path=\"$HOME/local/lib:$(opam config var lib)\"%" $FINDCONF >$FINDCONF.1
