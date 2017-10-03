324cd0e2ade04fc1fa2356121904334a5d2d7eb6
Louis Gesbert <louis.gesbert@ocamlpro.com>
2017-10-03 19:01:21 +0200

    Require latest ocaml-mccs

--- .travis-ci.sh
+++ .travis-ci.sh
@@ -12,7 +12,7 @@ install-bootstrap () {
     opam init --root=$OPAMBSROOT --yes --no-setup --compiler=$OCAML_VERSION
     eval $(opam config env --root=$OPAMBSROOT)
     if [ "$OPAM_TEST" = "1" ]; then
-        opam install ocamlfind ocamlbuild cohttp cohttp-lwt-unix 'lwt>=3.1.0' ssl cmdliner dose3 opam-file-format re jbuilder.1.0+beta12 mccs --yes
+        opam install ocamlfind ocamlbuild cohttp cohttp-lwt-unix 'lwt>=3.1.0' ssl cmdliner dose3 opam-file-format re jbuilder.1.0+beta12 'mccs>=1.1+3' --yes
         # Allow use of ocamlfind packages in ~/local/lib
         FINDCONF=$(ocamlfind printconf conf)
         sed "s%^path=.*%path=\"$HOME/local/lib:$(opam config var lib)\"%" $FINDCONF >$FINDCONF.1
