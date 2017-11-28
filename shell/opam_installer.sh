4352dfafa8c9eae84eb29d950039ab7af676e9ed
Louis Gesbert <louis.gesbert@ocamlpro.com>
2017-11-28 11:19:50 +0100

    Rewrite the opam_installer.sh script for ease & backups

--- shell/opam_installer.sh
+++ shell/opam_installer.sh
@@ -3,97 +3,248 @@
 set -ue
 
 # (c) Copyright Fabrice Le Fessant INRIA/OCamlPro 2013
-# (c) Copyright Louis Gesbert OCamlPro 2014-2015
+# (c) Copyright Louis Gesbert OCamlPro 2014-2017
 
-VERSION='1.2.2'
-
-default_ocaml=4.05.0
+VERSION='2.0.0~beta5'
+TAG=$(echo "$VERSION" | tr '~' '-')
+DEFAULT_BINDIR=/usr/local/bin
 
 usage() {
-cat <<EOF
-
-Usage:
-    ./opam_install BINDIR [COMP]
-
-    Download and installs the latest binary version of opam
-
-    BINDIR is the directory where it should be installed, e.g. /usr/local/bin
-    (it should be in your PATH).
-
-    COMP is an optional argument, specifying the initial version of OCaml you
-    want to use ($default_ocaml by default. You may use 'system' if you want to
-    use an ocaml compiler already present on your system).
-EOF
-    exit 1
+    echo "opam binary installer v.$VERSION"
+    echo "Downloads and installs a pre-compiled binary of opam $VERSION to the system."
+    echo "This can also be used to switch between opam versions"
+    echo
+    echo "Options:"
+    echo "    --no-backup            Don't attempt to backup the current opam root"
+    echo "    --backup               Force the backup the current opam root (even if it"
+    echo "                           is from the 2.0 branch already)"
+    echo "    --fresh                Create the opam $VERSION root from scratch"
+    echo "    --restore   VERSION    Restore a backed up opam binary and root"
+    echo
+    echo "The default is to backup if the current version of opam is 1.*, or when"
+    echo "using '--fresh'"
 }
 
-#
-#       Report an error and exit
-#
-PROGNAME=$0
-error() {
-    echo -n "`basename $PROGNAME`: " >&2
-    for s in "$@"; do echo $s; done
-    exit 1
-}
+RESTORE=
+NOBACKUP=
+FRESH=
 
+while [ $# -gt 0 ]; do
+    case "$1" in
+        --restore)
+            if [ $# -lt 2 ]; then echo "Option $1 requires an argument"; exit 2; fi
+            shift;
+            RESTORE=$1;;
+        --no-backup)
+            NOBACKUP=1;;
+        --backup)
+            NOBACKUP=0;;
+        --fresh)
+            FRESH=1;;
+        --help|-h)
+            usage; exit 0;;
+        *)
+            usage; exit 2;;
+    esac
+    shift
+done
 
-TMP=${TMPDIR:-/tmp}
+EXISTING_OPAM=$(command -v opam || echo)
+EXISTING_OPAMV=
+if [ -n "$EXISTING_OPAM" ]; then
+   EXISTING_OPAMV=$("$EXISTING_OPAM" --version || echo "unknown")
+fi
 
-dlerror () {
-    error "Couldn't download $url" \
-        "There may not yet be a binary release for your architecture or OS, sorry."
-}
+FRESH=${FRESH:-0}
 
-getopam() {
-    opamfile=$2
-    url=$1/$opamfile
+if [ -z "$NOBACKUP" ] && [ ! "$FRESH" = 1 ] && [ -z "$RESTORE" ]; then
+    case "$EXISTING_OPAMV" in
+        2.*) NOBACKUP=1;;
+        *) NOBACKUP=0;;
+    esac
+fi
 
-    if which wget >/dev/null; then
-        wget -q -O "$TMP/$opamfile" "$url" || dlerror
+OPAMROOT=${OPAMROOT:-$HOME/.opam}
+
+xsudo() {
+    local CMD=$1; shift
+    local DST
+    for DST in "$@"; do : ; done
+
+    local DSTDIR=$(dirname "$DST")
+    if [ ! -w "$DSTDIR" ]; then
+        echo "Write access to $DSTDIR required, using 'sudo'."
+        echo "Command: $CMD $@"
+        if [ "$CMD" = "install" ]; then
+            sudo "$CMD" -g root -o root "$@"
+        else
+            sudo "$CMD" "$@"
+        fi
     else
-        curl -s -L -o "$TMP/$opamfile" "$url" || dlerror
+        "$CMD" "$@"
     fi
 }
 
-if [ $# -lt 1 ] || [ $# -gt 2 ] || [ "${1#-}" != "$1" ]; then
-    echo "opam binary installer v. $VERSION"
-    usage
+if [ -n "$RESTORE" ]; then
+    OPAM=$(command -v opam)
+    OPAMV=$("$OPAM" --version)
+    OPAM_BAK="$OPAM.$RESTORE"
+    OPAMROOT_BAK="$OPAMROOT.$RESTORE"
+    if [ ! -e "$OPAM_BAK" ] || [ ! -d "$OPAMROOT_BAK" ]; then
+        echo "No backup of opam $RESTORE was found"
+        exit 1
+    fi
+    if [ "$NOBACKUP" = 1 ]; then
+        printf "## This will clear $OPAM and $OPAMROOT. Continue ? [Y/n] "
+        read R
+        case "$R" in
+            ""|"y"|"Y"|"yes")
+                xsudo rm -f "$OPAM"
+                rm -rf "$OPAMROOT";;
+            *) exit 1
+        esac
+    else
+        xsudo mv "$OPAM" "$OPAM.$OPAMV"
+        mv "$OPAMROOT" "$OPAMROOT.$OPAMV"
+    fi
+    xsudo mv "$OPAM_BAK" "$OPAM"
+    mv "$OPAMROOT_BAK" "$OPAMROOT"
+    printf "## Opam $RESTORE and its root were restored."
+    if [ "$NOBACKUP" = 1 ]; then echo
+    else echo " Opam $OPAMV was backed up."
+    fi
+    exit 0
 fi
 
-BINDIR=$1
-COMP=${2:-$default_ocaml}
+TMP=${TMPDIR:-/tmp}
 
-file="opam-$VERSION-$(uname -m || echo unknown)-$(uname -s || echo unknown)"
+ARCH=$(uname -m || echo unknwon)
+case "$ARCH" in
+    x86|i?86) ARCH="i686";;
+    x86_64|amd64) ARCH="x86_64";;
+    ppc|powerpc|ppcle) ARCH="ppc";;
+    aarch64_be|aarch64|armv8b|armv8l) ARCH="arm64";;
+    armv5*|armv6*|earmv6*|armv7*|earmv7*) ARCH="armhf";;
+    *) ARCH=$(echo "$ARCH" | awk '{print tolower($0)}')
+esac
 
-echo Downloading opam...
-getopam "https://github.com/ocaml/opam/releases/download/$VERSION" $file
+OS=$(uname -s || echo unknown)
+case "$OS" in
+    darwin|macos) OS=darwin;;
+    *) OS=$(echo "$OS" | awk '{print tolower($0)}')
+esac
 
-mkdir -p "$BINDIR" 2>/dev/null || true
-if [ ! -w "$BINDIR" ]; then
-    echo "You don't have write access to $BINDIR: sudo may ask for your password"
-    if [ ! -d "$BINDIR" ]; then sudo mkdir -p "$BINDIR"; fi
-    sudo install -g root -o root -m 755 $TMP/$file $BINDIR/opam
+OPAM_BIN="opam-${TAG}-${ARCH}-${OS}"
+OPAM_BIN_URL="https://github.com/ocaml/opam/releases/download/${TAG}/${OPAM_BIN}"
+
+download() {
+    if command -v wget >/dev/null; then wget -q -O "$@"
+    else curl -s -L -o "$@"
+    fi
+}
+
+if [ ! -e "$TMP/$OPAM_BIN" ]; then
+    echo "## Downloading opam $VERSION for $OS on $ARCH..."
+
+    if ! download "$TMP/$OPAM_BIN" "$OPAM_BIN_URL"; then
+        echo "There may not yet be a binary release for your architecture or OS, sorry."
+        echo "See https://github.com/ocaml/opam/releases/tag/$TAG for pre-compiled binaries,"
+        echo "or run 'make cold' from https://github.com/ocaml/opam/archive/$TAG.tar.gz"
+        echo "to build from scratch"
+        exit 10
+    else
+        echo "## Downloaded."
+    fi
 else
-    install -m 755 $TMP/$file $BINDIR/opam
-fi
-rm -f $TMP/$file
-
-OPAM=$(which opam || echo "$BINDIR/opam")
-if [ "$OPAM" != "$BINDIR/opam" ]; then
-    echo "WARNING: you have a different version of opam installed at $OPAM"
-    echo "It is highly recommended that you remove it."
-    read -p "[press enter to continue]" x
-    OPAM="$BINDIR/opam"
+    echo "## Using already downloaded \"$TMP/$OPAM_BIN\""
 fi
 
-if [ "$(id -u)" = "0" ]; then
-    echo "Running as super-user: not running opam initialization."
-    echo "You'll want to run \"$OPAM init --comp $COMP\" as user"
-else
-    echo "Initializing with compiler $COMP"
-    "$OPAM" init --comp "$COMP"
+if [ -n "$EXISTING_OPAM" ]; then
+    DEFAULT_BINDIR=$(dirname "$EXISTING_OPAM")
 fi
 
-echo "Installation done. If you need to uninstall, simply remove $BINDIR/opam"
-echo "and ~/.opam"
+while true; do
+    printf "## Where should it be installed ? [$DEFAULT_BINDIR] "
+    read BINDIR
+    if [ -z "$BINDIR" ]; then BINDIR="$DEFAULT_BINDIR"; fi
+
+    if [ -d "$BINDIR" ]; then break
+    else
+        printf "## $BINDIR does not exist. Create ? [Y/n] "
+        read R
+        case "$R" in
+            ""|"y"|"Y"|"yes") break;;
+        esac
+    fi
+done
+
+if [ -e "$EXISTING_OPAM" ]; then
+    if [ "$NOBACKUP" = 1 ]; then
+        xsudo rm -f "$EXISTING_OPAM"
+    else
+        xsudo mv "$EXISTING_OPAM" "$EXISTING_OPAM.$EXISTING_OPAMV"
+        echo "## $EXISTING_OPAM backed up as $(basename $EXISTING_OPAM).$EXISTING_OPAMV"
+    fi
+fi
+
+if [ -d "$OPAMROOT" ]; then
+    if [ "$FRESH" = 1 ]; then
+        if [ "$NOBACKUP" = 1 ]; then
+            printf "## This will clear $OPAMROOT. Continue ? [Y/n] "
+            read R
+            case "$R" in
+                ""|"y"|"Y"|"yes")
+                    rm -rf "$OPAMROOT";;
+                *) exit 1
+            esac
+        else
+            mv "$OPAMROOT" "$OPAMROOT.$EXISTING_OPAMV"
+            echo "## $OPAMROOT backed up as $(basename $OPAMROOT).$EXISTING_OPAMV"
+        fi
+        echo "## opam $VERSION installed. Please run 'opam init' to get started"
+    elif [ ! "$NOBACKUP" = 1 ]; then
+        echo "## Backing up $OPAMROOT to $(basename $OPAMROOT).$EXISTING_OPAMV (this may take a while)"
+        if [ -e "$OPAMROOT.$EXISTING_OPAMV" ]; then
+            echo "ERROR: there is already a backup at $OPAMROOT.$EXISTING_OPAMV"
+            echo "Please move it away or run with --no-backup"
+        fi
+        FREE=$(df -k "$OPAMROOT" | awk 'NR>1 {print $4}')
+        NEEDED=$(du -s "$OPAMROOT" | awk '{print $1}')
+        if ! [ $NEEDED -lt $FREE ]; then
+            echo "Error: not enough free space to backup. You can retry with --no-backup,"
+            echo "--fresh, or remove '$OPAMROOT'"
+            exit 1
+        fi
+        cp -a "$OPAMROOT" "$OPAMROOT.$EXISTING_OPAMV"
+        echo "## $OPAMROOT backed up as $(basename $OPAMROOT).$EXISTING_OPAMV"
+    fi
+fi
+
+xsudo install -m 755 "$TMP/$OPAM_BIN" "$BINDIR/opam"
+echo "## opam $VERSION installed to $BINDIR"
+
+if [ ! "$FRESH" = 1 ]; then
+    echo "## Converting the opam root format"
+    "$BINDIR/opam" update --yes >/dev/null
+    echo "## Updating the repositories"
+    "$BINDIR/opam" update
+fi
+
+WHICH=$(command -v opam || echo notfound)
+
+case "$WHICH" in
+    "$BINDIR/opam") ;;
+    notfound) echo "## Remember to add $BINDIR to your PATH";;
+    *)
+        echo "## WARNING: 'opam' command found in PATH does not match the installed one:"
+        echo "   - Installed: '$BINDIR/opam'"
+        echo "   - Found:     '$WHICH'"
+        echo "Make sure to remove the second or fix your PATH to use the new opam"
+        echo
+esac
+
+if [ ! "$NOBACKUP" = 1 ]; then
+    echo "## Run this script again with '--restore $EXISTING_OPAMV' to revert."
+fi
+
+rm -f $TMP/$OPAM_BIN
