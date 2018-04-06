478a4a4dc5dc8b7d23c450841adf6fe9c6ca2d49
Louis Gesbert <louis.gesbert@ocamlpro.com>
2018-04-06 10:59:59 +0200

    Some optimisations (OpamSolver)
    This also puts back the Cudf formulas to follow the order of the opam formulas

--- src/solver/opamSolver.mli
+++ src/solver/opamSolver.mli
@@ -52,10 +52,11 @@ val print_solution:
 val cudf_versions_map: universe -> package_set -> int OpamPackage.Map.t
 
 (** Creates a CUDF universe from an OPAM universe, including the given
-    packages *)
+    packages. Evaluation of the first 3 arguments is staged *)
 val load_cudf_universe:
-  ?depopts:bool -> build:bool -> post:bool ->
-  universe -> ?version_map:int package_map -> package_set -> Cudf.universe
+  universe -> ?version_map:int package_map -> package_set ->
+  ?depopts:bool -> build:bool -> post:bool -> unit ->
+  Cudf.universe
 
 (**  Build a request *)
 val request:
