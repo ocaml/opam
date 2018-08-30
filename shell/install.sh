22b65536df13ef1f7671e6f81d37f80600b7e727
Nicola Mometto <me@bronsa.net>
2018-08-30 17:58:42 +0100

    Don't continue with checksum check if openssl sha256 is unavailable (#3516)
    Don't continue with checksum check if openssl sha512 is unavailable

--- shell/install.sh
+++ shell/install.sh
@@ -158,7 +158,7 @@ bin_sha512() {
 }
 
 check_sha512() {
-  if command -v openssl > /dev/null; then
+  if command -v openssl > /dev/null && [ "$(openssl sha512 2>&1 < /dev/null)" = "cf83e1357eefb8bdf1542850d66d8007d620e4050b5715dc83f4a921d36ce9ce47d0d13c5d85f2b0ff8318d2877eec2f63b931bd47417a81a538327af927da3e" ]; then
     sha512=`openssl sha512 "$TMP/$OPAM_BIN" 2> /dev/null | cut -f 2 -d ' '`
     check=`bin_sha512`
     test "x$sha512" = "x$check"
