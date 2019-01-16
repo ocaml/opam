a92c80bfb4654581427a49bd7a99a69a49bc44d4
R. Boujbel <raja.boujbel@ocamlpro.com>
2019-01-16 19:19:04 +0100

    switch: allow local compiler switch creation (#3720)

--- src/client/opamSwitchCommand.mli
+++ src/client/opamSwitchCommand.mli
@@ -24,7 +24,9 @@ val install:
   ?synopsis:string ->
   ?repos:repository_name list ->
   update_config:bool ->
-  packages:atom conjunction -> switch ->
+  packages:atom conjunction -> 
+  ?local_compiler:bool ->
+  switch ->
   unlocked global_state * rw switch_state
 
 (** Install a compiler's base packages *)
