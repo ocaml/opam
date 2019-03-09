87f9201c4ae8a7248dc83f8e849e9c4481a9440d
Gabriel Scherer <gabriel.scherer@gmail.com>
2019-03-10 00:33:29 +0100

    crowbar: add OpamHash support

--- src/crowbar/opamHash_crowbar.ml
+++ src/crowbar/opamHash_crowbar.ml
@@ -8,7 +8,19 @@
 (*                                                                        *)
 (**************************************************************************)
 
-let () =
-  OpamCudf_crowbar.check ();
-  OpamFilename_crowbar.check ();
-  OpamHash_crowbar.check ();
+open OpamHash
+open! Crowbar
+open OpamCrowbar
+
+let kind : kind gen = choose [
+    const `MD5;
+    const `SHA256;
+    const `SHA512;
+]
+
+let hash = map [kind; bytes] @@ fun kind string -> 
+  OpamHash.compute_from_string ~kind string
+
+let check () =
+  check_json_roundtrip ~name:"OpamHash.t"
+    hash (=) OpamHash.to_json OpamHash.of_json;
