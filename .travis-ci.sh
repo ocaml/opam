3318d80e2f2c249dcabff0f8fcfecd72870b260c
Anton Kochkov <xvilka@gmail.com>
2019-09-29 17:49:49 +0800

    Add OCaml 4.08 and 4.09 in Travis
    Do not use -no-graph for >=4.08
    
    Do not use -no-debugger for >=4.08
    
    Do not use -no-ocamldoc for >=4.08
    
    Address feedback
    
    Address feedback [2]

--- .travis-ci.sh
+++ .travis-ci.sh
@@ -159,16 +159,24 @@ EOF
     else
       if [[ ! -x ~/local/bin/ocaml ]] ; then
         echo -en "travis_fold:start:ocaml\r"
-        wget http://caml.inria.fr/pub/distrib/ocaml-${OCAML_VERSION%.*}/ocaml-$OCAML_VERSION.tar.gz
-        tar -xzf ocaml-$OCAML_VERSION.tar.gz
-        cd ocaml-$OCAML_VERSION
+        wget "http://caml.inria.fr/pub/distrib/ocaml-${OCAML_VERSION%.*}/ocaml-$OCAML_VERSION.tar.gz"
+        tar -xzf "ocaml-$OCAML_VERSION.tar.gz"
+        cd "ocaml-$OCAML_VERSION"
         if [[ $OPAM_TEST -ne 1 ]] ; then
-          CONFIGURE_SWITCHES="-no-ocamldoc"
-          if [[ "$OCAML_VERSION" != "4.02.3" ]] ; then
-            CONFIGURE_SWITCHES="$CONFIGURE_SWITCHES -no-ocamlbuild"
+          if [[ -e configure.ac ]]; then
+            CONFIGURE_SWITCHES="--disable-debugger --disable-debug-runtime --disable-ocamldoc"
+            if [[ ${OCAML_VERSION%.*} = '4.08' ]]; then
+              CONFIGURE_SWITCHES="$CONFIGURE_SWITCHES --disable-graph-lib"
+            fi
+          else
+            CONFIGURE_SWITCHES="-no-graph -no-debugger -no-ocamldoc"
+            if [[ "$OCAML_VERSION" != "4.02.3" ]] ; then
+              CONFIGURE_SWITCHES="$CONFIGURE_SWITCHES -no-ocamlbuild"
+            fi
+
           fi
         fi
-        ./configure --prefix ~/local -no-graph -no-debugger ${CONFIGURE_SWITCHES:-}
+        ./configure --prefix ~/local ${CONFIGURE_SWITCHES:-}
         if [[ $OPAM_TEST -eq 1 ]] ; then
           make -j 4 world.opt
         else
