d8ef3836f90872e4b8b38d1ba3c76dd290bedffa
Anton Kochkov <anton.kochkov@gmail.com>
2019-11-14 18:39:22 +0800

    Address feedback [2]

--- .travis-ci.sh
+++ .travis-ci.sh
@@ -162,19 +162,21 @@ EOF
         wget "http://caml.inria.fr/pub/distrib/ocaml-${OCAML_VERSION%.*}/ocaml-$OCAML_VERSION.tar.gz"
         tar -xzf "ocaml-$OCAML_VERSION.tar.gz"
         cd "ocaml-$OCAML_VERSION"
-        CONFIGURE_SWITCHES=""
         if [[ $OPAM_TEST -ne 1 ]] ; then
           if [[ -e configure.ac ]]; then
-            CONFIGURE_SWITCHES="$CONFIGURE_SWITCHES --disable-debugger --disable-ocamldoc --disable-graphics-libs"
+            CONFIGURE_SWITCHES="--disable-debugger --disable-debug-runtime --disable-ocamldoc"
+            if [[ ${OCAML_VERSION%.*} = '4.08' ]]; then
+              CONFIGURE_SWITCHES="$CONFIGURE_SWITCHES --disable-graph-lib"
+            fi
           else
+            CONFIGURE_SWITCHES="-no-graph -no-debugger -no-ocamldoc"
             if [[ "$OCAML_VERSION" != "4.02.3" ]] ; then
               CONFIGURE_SWITCHES="$CONFIGURE_SWITCHES -no-ocamlbuild"
             fi
-            CONFIGURE_SWITCHES="$CONFIGURE_SWITCHES -no-graph -no-debugger -no-ocamldoc"
 
           fi
         fi
-        ./configure --prefix ~/local ${CONFIGURE_SWITCHES:-}
+        ./configure --prefix ~/local $CONFIGURE_SWITCHES
         if [[ $OPAM_TEST -eq 1 ]] ; then
           make -j 4 world.opt
         else
