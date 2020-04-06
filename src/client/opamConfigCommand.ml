9b9f1013c3c1c65220a30d0de85ae6e37666827a
Raja Boujbel <raja.boujbel@ocamlpro.com>
2020-04-06 17:28:20 +0200

    option: to get scope, lookup on switch first

--- src/client/opamConfigCommand.ml
+++ src/client/opamConfigCommand.ml
@@ -896,9 +896,9 @@ let get_scope field =
     with Invalid_argument _ -> field
   in
   let find l = OpamStd.List.find_opt (fun (f,_) -> f = field) l in
-  if find OpamFile.Config.fields <> None then
-    `Global
-  else if find OpamFile.Switch_config.fields <> None
-       || find OpamFile.Switch_config.sections <> None then
+  if find OpamFile.Switch_config.fields <> None
+  || find OpamFile.Switch_config.sections <> None then
     `Switch
+  else if find OpamFile.Config.fields <> None then
+    `Global
   else `None field
