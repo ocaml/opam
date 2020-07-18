48665f528533e8bcde6dc30825b3e9c7ae15e0c4
Kate <kit.ty.kate@disroot.org>
2020-07-18 03:39:42 +0100

    Fix a crash in sandbox.sh when the XDG cache directory does not exist

--- src/state/shellscripts/bwrap.sh
+++ src/state/shellscripts/bwrap.sh
@@ -78,9 +78,9 @@ add_ccache_mount() {
 add_dune_cache_mount() {
   u_cache=${XDG_CACHE_HOME:-$HOME/.cache}
   u_dune_cache=$u_cache/dune
-  cache=$(readlink -f "$u_cache")
+  cache=$(readlink -m "$u_cache")
   dune_cache=$cache/dune
-  dune_cache=$(readlink -f $u_dune_cache)
+  dune_cache=$(readlink -m $u_dune_cache)
   mkdir -p ${dune_cache}
   add_mount rw $u_dune_cache $dune_cache
 }
