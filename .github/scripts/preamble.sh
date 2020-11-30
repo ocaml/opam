3ffa9ae7c4dbfd628626f96d29ff03b1a39e6426
Raja Boujbel <raja.boujbel@ocamlpro.com>
2020-11-30 13:56:27 +0100

    Github Actions for linux & macos
    Divided in several jobs:
     * cache for ocaml system compilation
     * cache for src_ext archives retrieving
     * build on linux 4.0.2.3 -> 4.11.1 & macos 4.11.1
     * test (opam-rt) on linux/macos 4.11.1 with an embedded compiler build cache & an opam-rt cache
     * upgrade 1.2 -> 2.0 test on linux 4.11.1, with an embedded cache for opam 1.2 root
     * make cold test

---
+++ .github/scripts/preamble.sh
@@ -0,0 +1,46 @@
+
+CWD=$PWD
+CACHE=~/.cache
+CACHE=`eval echo $CACHE`
+echo "Cache -> $CACHE"
+OCAML_LOCAL=$CACHE/ocaml-local
+OPAM_LOCAL=$CACHE/opam-local
+PATH=$OPAM_LOCAL/bin:$OCAML_LOCAL/bin:$PATH; export PATH
+
+OPAM_COLD=${OPAM_COLD:-0}
+OPAM_TEST=${OPAM_TEST:-0}
+OPAM_UPGRADE=${OPAM_UPGRADE:-0}
+
+OPAMBSSWITCH=opam-build
+
+BRANCH=${GITHUB_REF##*/}
+
+git config --global user.email "travis@example.com"
+git config --global user.name "Travis CI"
+git config --global gc.autoDetach false
+
+# used only for TEST jobs
+init-bootstrap () {
+  if [ "$OPAM_TEST" = "1" ]; then
+    set -e
+    export OPAMROOT=$OPAMBSROOT
+    # The system compiler will be picked up
+    opam init --yes --no-setup git+https://github.com/ocaml/opam-repository#$OPAM_REPO_SHA
+    eval $(opam env)
+#    opam update
+    CURRENT_SWITCH=$(opam config var switch)
+    if [[ $CURRENT_SWITCH != "default" ]] ; then
+      opam switch default
+      eval $(opam env)
+      opam switch remove $CURRENT_SWITCH --yes
+    fi
+
+    opam switch create $OPAMBSSWITCH ocaml-system
+    eval $(opam env)
+    # extlib is installed, since UChar.cmi causes problems with the search
+    # order. See also the removal of uChar and uTF8 in src_ext/jbuild-extlib-src
+    opam install . --deps-only --yes
+
+    rm -f "$OPAMBSROOT"/log/*
+  fi
+}
