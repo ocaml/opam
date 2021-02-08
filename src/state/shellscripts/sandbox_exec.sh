c58ce6df6ecd74da4bd540d50fed19d2350c1afb
Sora Morimoto <sora@morimoto.io>
2021-02-09 05:17:34 +0900

    Fix the conflict with the environment variable name used by dune
    Signed-off-by: Sora Morimoto <sora@morimoto.io>

--- src/state/shellscripts/sandbox_exec.sh
+++ src/state/shellscripts/sandbox_exec.sh
@@ -49,9 +49,9 @@ add_ccache_mount() {
 }
 
 add_dune_cache_mount() {
-  DUNE_CACHE=${XDG_CACHE_HOME:-$HOME/.cache}/dune
-  mkdir -p "${DUNE_CACHE}"
-  add_mounts rw "$DUNE_CACHE"
+  dune_cache=${XDG_CACHE_HOME:-$HOME/.cache}/dune
+  mkdir -p "${dune_cache}"
+  add_mounts rw "$dune_cache"
  }
 
 # When using opam variable that must be defined at action time, add them also
