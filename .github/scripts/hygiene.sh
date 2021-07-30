5b58a5db49ef423d2199266f78035ac6ae77eb76
David Allsopp <david.allsopp@metastack.com>
2021-07-30 12:26:23 +0100

    Fix install.sh check in GHA

--- .github/scripts/hygiene.sh
+++ .github/scripts/hygiene.sh
@@ -84,43 +84,45 @@ esac
 # Check install.sh
 ###
 
-if [ "$GITHUB_EVENT_NAME" = "pull_request" ] ; then
-  (set +x ; echo -en "::group::check install.sh\r") 2>/dev/null
+if [[ $GITHUB_EVENT_NAME = 'pull_request' ]]; then
   if ! git diff "$BASE_REF_SHA..$PR_REF_SHA" --name-only --exit-code -- shell/install.sh > /dev/null ; then
-    echo "shell/install.sh updated - checking it"
+    echo '::group::shell/install.sh updated - checking it'
     eval $(grep '^\(OPAM_BIN_URL_BASE\|DEV_VERSION\|VERSION\)=' shell/install.sh)
-    echo "OPAM_BIN_URL_BASE=$OPAM_BIN_URL_BASE"
-    echo "VERSION = $VERSION"
-    echo "DEV_VERSION = $DEV_VERSION"
-    for VERSION in $DEV_VERSION $VERSION; do
-      eval $(grep '^TAG=' shell/install.sh)
-      echo "TAG = $TAG"
-      ARCHES=0
+    echo "OPAM_BIN_URL_BASE = $OPAM_BIN_URL_BASE"
+    echo "VERSION = $VERSION; DEV_VERSION = $DEV_VERSION"
+    if [[ $DEV_VERSION != $VERSION ]]; then
+      ARCHES=2
+    else
+      ARCHES=1
+    fi
+    current_tag=''
 
-      while read -r key sha
-      do
-        ARCHES=1
-        URL="$OPAM_BIN_URL_BASE$TAG/opam-$TAG-$key"
-        echo "Checking $URL"
-        check=$(curl -Ls "$URL" | sha512sum | cut -d' ' -f1)
-        if [ "$check" = "$sha" ] ; then
-          echo "Checksum as expected ($sha)"
-        else
-          echo -e "[\e[31mERROR\e[0m] Checksum downloaded: $check"
-          echo -e "[\e[31mERROR\e[0m] Checksum install.sh: $sha"
-          ERROR=1
+    while read -r line tag platform sha ; do
+      if [[ $tag != $current_tag ]]; then
+        current_tag="$tag"
+        if [[ $tag = $VERSION || $tag = $DEV_VERSION ]]; then
+          ((ARCHES--))
         fi
-      done < <(sed -ne "s/.*opam-$TAG-\([^)]*\).*\"\([^\"]*\)\".*/\1 \2/p" shell/install.sh)
-    done
-    if [ $ARCHES -eq 0 ] ; then
-      echo "[\e[31mERROR\e[0m] No sha512 checksums were detected in shell/install.sh"
-      echo "That can't be right..."
+        echo "🐪 Checking binaries for opam ${tag//-/\~}"
+      fi
+      URL="$OPAM_BIN_URL_BASE$tag/opam-$tag-$platform"
+      echo "Downloading $URL"
+      check=$(curl -Ls "$URL" | sha512sum | cut -d' ' -f1)
+      if [[ $check = $sha ]] ; then
+        echo "         as expected ($sha)"
+      else
+        echo "::error file=shell/install.sh,line=$line::Incorrect checksum; got $check"
+        ERROR=1
+      fi
+    done < <(gawk 'match($0, /^ *opam-([0-9]\.[^)]*)-([^-)]*-[^-)]*)) *echo "([^"]*)".*/, m) { print NR " " m[1] " " m[2] " " m[3]; }' shell/install.sh)
+    if [[ $ARCHES -ne 0 ]] ; then
+      echo '::error file=shell/install.sh::No sha512 checksums were detected?!'
       ERROR=1
     fi
+    echo '::endgroup::'
   else
-    echo "No changes in install.sh"
+    echo 'No changes in shell/install.sh'
   fi
-  (set +x ; echo -en "::endgroup::check install.sh\r") 2>/dev/null
 fi
 
 
