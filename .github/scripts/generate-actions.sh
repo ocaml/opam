0ce9daca8401a436e7e527f5717a4c6eb2306b8b
Raja Boujbel <raja.boujbel@ocamlpro.com>
2021-10-13 19:11:09 +0200

    gha: depext: add opam repo cache

--- .github/scripts/generate-actions.sh
+++ .github/scripts/generate-actions.sh
@@ -108,6 +108,7 @@ set -eux
 # For systems that don't have an up to date compiler, to avoid ocaml-secondary
 echo 'default-invariant: [ "ocaml" {>= "4.09.0"} ]' > /opam/opamrc
 opam init --no-setup --disable-sandboxing --bare --config /opam/opamrc git+$OPAM_REPO#$OPAM_REPO_SHA
+echo 'archive-mirrors: "https://opam.ocaml.org/cache"' >> \$OPAMROOT/config
 opam switch create this-opam ocaml
 
 # Workdir is /github/workpaces
