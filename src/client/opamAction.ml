35a4481566b749196f548bf67b653e5e41d1412d
Raja Boujbel <raja.boujbel@ocamlpro.com>
2021-11-24 18:25:19 +0100

    version pin: don't read opam file in source

--- src/client/opamAction.ml
+++ src/client/opamAction.ml
@@ -245,11 +245,12 @@ let download_package st nv =
   let dir = OpamSwitchState.source_dir st nv in
   if OpamPackage.Set.mem nv st.pinned &&
      OpamFilename.exists_dir dir &&
-     OpamStd.Option.Op.(
-       OpamPinned.find_opam_file_in_source nv.name dir >>|
-       OpamFile.OPAM.safe_read >>=
-       OpamFile.OPAM.version_opt)
-     = Some nv.version
+     (OpamSwitchState.is_version_pinned st (OpamPackage.name nv) ||
+      OpamStd.Option.Op.(
+        OpamPinned.find_opam_file_in_source nv.name dir >>|
+        OpamFile.OPAM.safe_read >>=
+        OpamFile.OPAM.version_opt)
+      = Some nv.version)
   then Done None
   else
   let print_action msg =
