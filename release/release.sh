65daa33813fa1eefbf4c2958bf98b29a7d5201bc
Kate <kit.ty.kate@disroot.org>
2021-12-05 20:18:26 +0000

    Release script: Always reset the openbsd image

--- release/release.sh
+++ release/release.sh
@@ -50,7 +50,8 @@ if [[ $# -eq 0 || " $* " =~ " builds " ]]; then
   [ -f ${OUTDIR}/opam-$TAG-x86_64-macos ] || make GH_USER="${GH_USER}" TAG="$TAG" macos-local ARCH=x86_64 REMOTE_DIR=opam-release-$TAG &
   [ -f ${OUTDIR}/opam-$TAG-arm64-macos ] || make GH_USER="${GH_USER}" TAG="$TAG" macos-local ARCH=arm64 REMOTE_DIR=opam-release-$TAG &
   [ -f ${OUTDIR}/opam-$TAG-x86_64-openbsd ] || \
-    ( ([ -f ./qemu-base-images ] || (git clone https://github.com/kit-ty-kate/qemu-base-images.git && qemu-img convert -O raw ./qemu-base-images/OpenBSD-7.0-amd64.qcow2 ./qemu-base-images/OpenBSD-7.0-amd64.raw)) &&
+    ( ([ -d ./qemu-base-images ] || (git clone https://github.com/kit-ty-kate/qemu-base-images.git)) &&
+      qemu-img convert -O raw ./qemu-base-images/OpenBSD-7.0-amd64.qcow2 ./qemu-base-images/OpenBSD-7.0-amd64.raw &&
       (ssh -p 9999 root@localhost true || (qemu-system-x86_64 -drive "file=./qemu-base-images/OpenBSD-7.0-amd64.raw,format=raw" -nic "user,hostfwd=tcp::9999-:22" -m 1G & sleep 60)) &&
       ssh -p 9999 root@localhost "pkg_add gmake curl bzip2" &&
       make GH_USER="${GH_USER}" TAG="$TAG" qemu QEMU_PORT=9999 REMOTE_MAKE=gmake REMOTE_DIR=opam-release-$TAG &&
