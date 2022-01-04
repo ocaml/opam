4804e5fbc11649e7d99c4be60e6b5882d05ec9af
Raja Boujbel <raja.boujbel@ocamlpro.com>
2022-01-04 21:09:37 +0100

    list: add pick, on the same pattern than pick_assoc

--- src/core/opamStd.ml
+++ src/core/opamStd.ml
@@ -176,6 +176,15 @@ module OpamList = struct
     |  [] -> None
     | (a,b)::l -> if eq a x then Some b else assoc_opt eq x l
 
+  let pick f l =
+    let rec aux acc = function
+      | [] -> None, l
+      | x::l ->
+        if f x then Some x, List.rev_append acc l
+        else aux (x::acc) l
+    in
+    aux [] l
+
   let pick_assoc eq x l =
     let rec aux acc = function
       | [] -> None, l
