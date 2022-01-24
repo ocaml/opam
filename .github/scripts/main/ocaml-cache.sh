c3f9e6253b82ccc65b0e4479c100255a0586db67
David Allsopp <david.allsopp@metastack.com>
2022-01-24 16:29:20 +0000

    Fake ocamldoc in the caches
    Zarith contains an unecessary dependency on ocamldoc in its configure
    script - instead of installing ocamlbuild, a fake error-triggering
    script is installed in ocamldoc's place.

--- .github/scripts/main/ocaml-cache.sh
+++ .github/scripts/main/ocaml-cache.sh
@@ -8,18 +8,12 @@ tar -xzf "ocaml-$OCAML_VERSION.tar.gz"
 cd "ocaml-$OCAML_VERSION"
 if [[ $OPAM_TEST -ne 1 ]] ; then
   if [[ -e configure.ac ]]; then
-    CONFIGURE_SWITCHES="--disable-debugger --disable-debug-runtime"
-    if [ "$SOLVER" != "z3" ]; then
-      CONFIGURE_SWITCHES="$CONFIGURE_SWITCHES --disable-ocamldoc"
-    fi
+    CONFIGURE_SWITCHES="--disable-debugger --disable-debug-runtime --disable-ocamldoc"
     if [[ ${OCAML_VERSION%.*} = '4.08' ]]; then
       CONFIGURE_SWITCHES="$CONFIGURE_SWITCHES --disable-graph-lib"
     fi
   else
-    CONFIGURE_SWITCHES="-no-graph -no-debugger"
-    if [ "$SOLVER" != "z3" ]; then
-      CONFIGURE_SWITCHES="$CONFIGURE_SWITCHES -no-ocamldoc"
-    fi
+    CONFIGURE_SWITCHES="-no-graph -no-debugger -no-ocamldoc"
     if [[ ${OCAML_VERSION%.*} = '4.08' ]]; then
       CONFIGURE_SWITCHES="$CONFIGURE_SWITCHES --disable-graph-lib"
     fi
@@ -39,6 +33,13 @@ else
 fi
 
 make install
+echo > "$OCAML_LOCAL/bin/ocamldoc" <<"EOF"
+#!/bin/sh
+
+echo 'ocamldoc is not supposed to be called'>&2
+exit 1
+EOF
+chmod +x "$OCAML_LOCAL/bin/ocamldoc"
 
 cd ..
 rm -rf "ocaml-$OCAML_VERSION"
