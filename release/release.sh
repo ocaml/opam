7d0d1985183c24a05fd6c416c0067468944a9531
Kate <kit.ty.kate@disroot.org>
2022-08-12 21:26:36 +0100

    Simplify the release script

--- release/release.sh
+++ release/release.sh
@@ -1,9 +1,6 @@
 #!/usr/bin/env bash
 set -uex
 
-# Change this to your github user if you only want to test the script
-GH_USER=ocaml
-
 usage() {
     echo "Usage: $0 TAG"
     exit 1
@@ -27,6 +24,7 @@ cd "$DIR"
 LC_ALL=C
 CWD=$(pwd)
 JOBS=$(sysctl -n hw.ncpu)
+JOBS=$(echo "${JOBS} / 1.5" | bc)
 SSH="ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no"
 
 OUTDIR="out/$TAG"
@@ -41,21 +39,23 @@ qemu_build() {
 
   if ! ${SSH} -p "${port}" root@localhost true; then
       qemu-img convert -O raw "./qemu-base-images/${image}.qcow2" "./qemu-base-images/${image}.raw"
-      "qemu-system-${arch}" -drive "file=./qemu-base-images/${image}.raw,format=raw" -nic "user,hostfwd=tcp::${port}-:22" -m 2G -smp "${JOBS}" &
+      "qemu-system-${arch}" -drive "file=./qemu-base-images/${image}.raw,format=raw" -nic "user,hostfwd=tcp::${port}-:22" -m 2G &
       sleep 60
   fi
   ${SSH} -p "${port}" root@localhost "${install}"
-  make GH_USER="${GH_USER}" TAG="$TAG" JOBS="${JOBS}" qemu QEMU_PORT="${port}" REMOTE_MAKE="${make}" REMOTE_DIR=opam-release-$TAG
+  # NOTE: JOBS=1 because qemu does not support proper multithreading from arm64 to x86_64 yet because of memory model differences.
+  # See https://wiki.qemu.org/Features/tcg-multithread
+  make TAG="$TAG" JOBS=1 qemu QEMU_PORT="${port}" REMOTE_MAKE="${make}" REMOTE_DIR=opam-release-$TAG
   ${SSH} -p "${port}" root@localhost "shutdown -p now"
 }
 
-make GH_USER="${GH_USER}" TAG="$TAG" GIT_URL="https://github.com/${GH_USER}/opam.git" "${OUTDIR}/opam-full-$TAG.tar.gz"
-make "-j${JOBS}" GH_USER="${GH_USER}" TAG="$TAG" x86_64-linux
-make "-j${JOBS}" GH_USER="${GH_USER}" TAG="$TAG" i686-linux
-make "-j${JOBS}" GH_USER="${GH_USER}" TAG="$TAG" armhf-linux
-make "-j${JOBS}" GH_USER="${GH_USER}" TAG="$TAG" arm64-linux
-[ -f "${OUTDIR}/opam-$TAG-x86_64-macos" ] || make GH_USER="${GH_USER}" TAG="$TAG" JOBS="${JOBS}" macos-local MACOS_ARCH=x86_64 REMOTE_DIR=opam-release-$TAG GIT_URL="$CWD/.."
-[ -f "${OUTDIR}/opam-$TAG-arm64-macos" ] || make GH_USER="${GH_USER}" TAG="$TAG" JOBS="${JOBS}" macos-local MACOS_ARCH=arm64 REMOTE_DIR=opam-release-$TAG GIT_URL="$CWD/.."
+make JOBS="${JOBS}" TAG="$TAG" "${OUTDIR}/opam-full-$TAG.tar.gz"
+make JOBS="${JOBS}" TAG="$TAG" x86_64-linux
+make JOBS="${JOBS}" TAG="$TAG" i686-linux
+make JOBS="${JOBS}" TAG="$TAG" armhf-linux
+make JOBS="${JOBS}" TAG="$TAG" arm64-linux
+[ -f "${OUTDIR}/opam-$TAG-x86_64-macos" ] || make TAG="$TAG" JOBS="${JOBS}" macos-local MACOS_ARCH=x86_64 REMOTE_DIR=opam-release-$TAG GIT_URL="$CWD/.."
+[ -f "${OUTDIR}/opam-$TAG-arm64-macos" ] || make TAG="$TAG" JOBS="${JOBS}" macos-local MACOS_ARCH=arm64 REMOTE_DIR=opam-release-$TAG GIT_URL="$CWD/.."
 [ -d ./qemu-base-images ] || git clone https://gitlab.com/kit-ty-kate/qemu-base-images.git
 [ -f "${OUTDIR}/opam-$TAG-x86_64-openbsd" ] || qemu_build 9999 OpenBSD-7.0-amd64 "pkg_add gmake curl bzip2" gmake x86_64 &
 [ -f "${OUTDIR}/opam-$TAG-x86_64-freebsd" ] || qemu_build 9998 FreeBSD-13.0-RELEASE-amd64 "pkg install -y gmake curl bzip2" gmake x86_64 &
