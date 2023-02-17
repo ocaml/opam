8b294e78295d783a199de277aa5828c520517fa8
Kate <kit-ty-kate@outlook.com>
2023-02-17 20:54:20 +0000

    Reftests: Clone opam-repository once and use it locally instead of downloading individual commits
    This is faster on slower hardware but 20 seconds slower on faster hardware (2.50s on M1Pro vs. 2.30s before)
    The size of artefacts in _build also jump from 5.4GB to 7.1GB sadly.

--- tests/reftests/gen.ml
+++ tests/reftests/gen.ml
@@ -1,3 +1,5 @@
+module StringSet = Set.Make(String)
+
 let first_line ~path =
   let ic = open_in path in
   let s = input_line ic in
@@ -21,9 +23,6 @@ let diff_rule base_name ~condition =
 |}
     base_name condition base_name base_name condition base_name
 
-let tgz_name ~archive_hash =
-  Printf.sprintf "opam-archive-%s.tar.gz" archive_hash
-
 let repo_directory ~archive_hash =
   Printf.sprintf "opam-repo-%s" archive_hash
 
@@ -41,12 +40,19 @@ let run_rule ~base_name ~archive_hash ~condition =
    (run ./run.exe %%{exe:../../src/client/opamMain.exe.exe} %%{dep:%s.test} %%{read-lines:testing-env}))))
 |} (base_name^".out") (opamroot_directory ~archive_hash) condition base_name
 
-let archive_download_rule archive_hash =
+let archive_download_rule archive_hashes =
+  let init_local_branch archive_hash =
    Format.sprintf {|
+   (run git -C ./opam-repository.git branch local-branch-%s %s)|}
+     archive_hash archive_hash
+  in
+  Format.sprintf {|
 (rule
- (targets %s)
- (action (run curl --silent -Lo %%{targets} https://github.com/ocaml/opam-repository/archive/%s.tar.gz)))
-|} (tgz_name ~archive_hash) archive_hash
+ (targets opam-repository.git)
+ (action
+  (progn
+   (run git clone -q --bare --single-branch https://github.com/ocaml/opam-repository)%s)))
+|} (String.concat "" (List.map init_local_branch (StringSet.elements archive_hashes)))
 
 let default_repo_rule =
   Format.sprintf {|
@@ -65,10 +71,8 @@ let archive_unpack_rule archive_hash =
 (rule
   (targets %s)
   (action
-   (progn
-    (run mkdir -p %%{targets})
-    (run tar -C %%{targets} -xzf %%{dep:%s} --strip-components=1))))
-|} (repo_directory ~archive_hash) (tgz_name ~archive_hash)
+   (run git clone -q -b local-branch-%s --single-branch %%{dep:./opam-repository.git} %%{targets})))
+|} (repo_directory ~archive_hash) archive_hash
 
 let opam_init_rule archive_hash =
   Format.sprintf {|
@@ -81,8 +85,6 @@ let opam_init_rule archive_hash =
            --no-setup --bypass-checks --no-opamrc --bare
            file://%%{dep:%s})))))
 |} (opamroot_directory ~archive_hash) (repo_directory ~archive_hash)
-
-module StringSet = Set.Make(String)
 
 let () =
   let () = set_binary_mode_out stdout true in
@@ -114,9 +116,9 @@ let () =
   in
   print_string default_repo_rule;
   print_string (opam_init_rule null_hash);
+  print_string (archive_download_rule archive_hashes);
   StringSet.iter
     (fun archive_hash ->
-       print_string (archive_download_rule archive_hash);
        print_string (archive_unpack_rule archive_hash);
        print_string (opam_init_rule archive_hash)
     )
