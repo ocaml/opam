5702076c1fb4456f9f76f65e78b2f6c3308b97b4
David Allsopp <david.allsopp@metastack.com>
2024-02-11 13:28:02 +0000

    For the internal Cygwin, use --quiet-mode noinput

--- src/state/opamSysInteract.ml
+++ src/state/opamSysInteract.ml
@@ -306,7 +306,7 @@ module Cygwin = struct
          "--no-startmenu";
          "--no-write-registry";
          "--no-version-check";
-         "--quiet-mode";
+         "--quiet-mode"; "noinput";
        ] @
          match packages with
          | [] -> []
@@ -943,7 +943,7 @@ let install_packages_commands_t ?(env=OpamVariable.Map.empty) config sys_package
        stored in `sys-pkg-manager-cmd` field *)
     [`AsUser (OpamFilename.to_string (Cygwin.cygsetup ())),
      [ "--root"; (OpamFilename.Dir.to_string (Cygwin.cygroot config));
-       "--quiet-mode";
+       "--quiet-mode"; "noinput";
        "--no-shortcuts";
        "--no-startmenu";
        "--no-desktop";
