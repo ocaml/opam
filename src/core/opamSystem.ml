1f2e2f9b0de3b2ab56da185cbaec18b4449e9b06
Kate <kit-ty-kate@outlook.com>
2024-03-21 14:04:06 +0000

    Warn if GNU patch is not detected during opam update

--- src/core/opamSystem.ml
+++ src/core/opamSystem.ml
@@ -1495,6 +1495,33 @@ let translate_patch ~dir orig corrected =
   end;
   close_in ch
 
+let gpatch = lazy begin
+  let rec search_gpatch = function
+    | [] -> None
+    | patch_cmd::patch_cmds ->
+      match OpamProcess.run (make_command ~name:"patch" patch_cmd ["--version"]) with
+      | r ->
+        (match OpamProcess.is_success r, r.OpamProcess.r_stdout with
+         | true, full::_ when
+             OpamStd.String.is_prefix_of ~from:0 ~full "GNU patch " ->
+           Some patch_cmd
+         | _ ->
+           search_gpatch patch_cmds)
+      | exception _ -> search_gpatch patch_cmds
+  in
+  let default_cmd, other_cmds =
+    match OpamStd.Sys.os () with
+    | OpamStd.Sys.OpenBSD
+    | OpamStd.Sys.FreeBSD -> ("gpatch", ["patch"])
+    | _ -> ("patch", ["gpatch"])
+  in
+  match search_gpatch (default_cmd :: other_cmds) with
+  | Some gpatch -> gpatch
+  | None ->
+    OpamConsole.warning "Invalid patch utility. Please install GNU patch";
+    default_cmd
+end
+
 let patch ?(preprocess=true) ~dir p =
   if not (Sys.file_exists p) then
     (OpamConsole.error "Patch file %S not found." p;
@@ -1507,12 +1534,7 @@ let patch ?(preprocess=true) ~dir p =
     else
       p
   in
-  let patch_cmd =
-    match OpamStd.Sys.os () with
-    | OpamStd.Sys.OpenBSD
-    | OpamStd.Sys.FreeBSD -> "gpatch"
-    | _ -> "patch"
-  in
+  let patch_cmd = Lazy.force gpatch in
   make_command ~name:"patch" ~dir patch_cmd ["-p1"; "-i"; p'] @@> fun r ->
     if not (OpamConsole.debug ()) then Sys.remove p';
     if OpamProcess.is_success r then Done None
