3e82fac5d124506f9a1fa8e7749b3f975b9fcafa
Raja Boujbel <raja.boujbel@ocamlpro.com>
2024-07-09 18:36:55 +0200

    gha: update upload-artifact action to v4

--- .github/workflows/ci.ml
+++ .github/workflows/ci.ml
@@ -324,7 +324,7 @@ let main_build_job ~analyse_job ~cygwin_job ?section runner start_version ~oc ~w
          ~cond:(Predicate(true, EndsWith("matrix.host", "-pc-windows")))
          ~withs:[ ("name", Literal ["opam-exe-${{ matrix.host }}-${{ matrix.ocamlv }}-${{ matrix.build }}"]);
                   ("path", Literal ["D:\\Local\\bin\\opam.exe"; "D:\\Local\\bin\\opam-installer.exe"; "D:\\Local\\bin\\opam-putenv.exe"]) ]
-         "actions/upload-artifact@v3")
+         "actions/upload-artifact@v4")
     ++ only_on Windows (run "Test (basic - Cygwin)" ~cond:(Predicate(true, EndsWith("matrix.host", "-pc-cygwin"))) ["bash -exu .github/scripts/main/test.sh"])
     ++ only_on Windows (run "Test (basic - native Windows)" ~env:[("OPAMROOT", {|D:\a\opam\opam\.opam|})] ~shell:"cmd" ~cond:(Predicate(false, EndsWith("matrix.host", "-pc-cygwin")))
          ({|set Path=D:\Cache\ocaml-local\bin;%Path%|} ::
