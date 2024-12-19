0473072d7e812a0046c1f0525290520d73b45d9e
progressive-galib <125106402+progressive-galib@users.noreply.github.com>
2024-12-19 20:24:56 +0600

    Update src/client/opamLockCommand.ml based on suggestions of
    @kit-ty-kate
    
    Co-authored-by: Kate <kit-ty-kate@outlook.com>

--- src/client/opamLockCommand.ml
+++ src/client/opamLockCommand.ml
@@ -96,8 +96,7 @@ let select_packages atom_locs st =
                atoms
            in
            if missing <> [] then
-             (OpamConsole.error_and_exit
-                `Aborted
+             OpamConsole.error_and_exit `Not_found
                 "Skipping %s, dependencies are not satisfied in this switch, \
                  not installed packages are:\n%s"
                 (OpamPackage.to_string nv)
