de1183dd7d9af8960ce6dd0b590f3567f311d4c8
Kate <kit-ty-kate@exn.st>
2025-07-20 13:06:15 +0100

    Fix pinned depends being ignored on simulated pins

--- src/client/opamAuxCommands.ml
+++ src/client/opamAuxCommands.ml
@@ -545,6 +545,12 @@ let autopin st ?(simulate=false) ?quiet ?locked ?recurse ?subpath
     else
       OpamUpdate.dev_packages st ~working_dir:OpamPackage.Set.empty already_pinned
   in
+  let st =
+    if OpamClientConfig.(!r.ignore_pin_depends) then st else
+      OpamPackage.Set.fold (fun nv st ->
+          OpamPinCommand.handle_pin_depends st nv (OpamSwitchState.opam st nv))
+        (OpamPackage.Set.union pins already_pinned_set) st
+  in
   st, simulate_pinned_atoms pins atoms
 
 let check_and_revert_sandboxing root config =
