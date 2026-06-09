0865dbdd0d7002b916aeaa8abae98234c77ef72c
Raja Boujbel <raja.boujbel@ocamlpro.com>
2026-06-09 16:38:27 +0200

    fixup tar : add some error handling

--- src/repository/opamTar.ml
+++ src/repository/opamTar.ml
@@ -18,45 +18,50 @@ type archived_file_content = string
 
 let log ?level fmt = OpamConsole.log "TAR" ?level fmt
 
+exception Tar of archive * string
+let raise_error tar fmt =
+  Printf.ksprintf (fun str -> raise (Tar (tar, str))) fmt
+
 let rec safe_read fd buf off len =
   try Unix.read fd buf off len
   with Unix.Unix_error (Unix.EINTR, _, _) -> safe_read fd buf off len
 
-let rec run : type a. Unix.file_descr -> (a, _, _) Tar.t -> a = fun fd -> function
-  | Tar.Read len ->
-    let b = Bytes.create len in
-    let read = safe_read fd b 0 len in
-    if read = 0 then
-      failwith "unexpected end of file"
-    else if len = (read : int) then
-      Bytes.unsafe_to_string b
-    else
-      Bytes.sub_string b 0 read
-  | Tar.Really_read len ->
-    let rec loop fd buf offset len =
-      if offset < (len : int) then
-        let n = safe_read fd buf offset (len - offset) in
-        if n = 0 then
-          failwith "unexpected end of file"
-        else
-          loop fd buf (offset + n) len
-    in
-    let buf = Bytes.create len in
-    loop fd buf 0 len;
-    Bytes.unsafe_to_string buf
-  | Tar.Return (Ok x) -> x
-  | Tar.Return (Error e) ->
-    let error =
-      match e with
-      | `Fatal e -> Format.asprintf "Fatal: %a" Tar.pp_error e
-      | `Eof -> "EOF"
-      | `Gz s -> "gz: "^s
-    in
-    failwith error
-  | Tar.High _ | Tar.Write _ | Tar.Seek _ -> assert false
-  | Tar.Bind (x, f) -> run fd (f (run fd x))
+let run archive =
+  let raise_error fmt = raise_error archive fmt in
+  let rec run : type a. Unix.file_descr -> (a, _, _) Tar.t -> a = fun fd -> function
+    | Tar.Read len ->
+      let b = Bytes.create len in
+      let read = safe_read fd b 0 len in
+      if read = 0 then
+        raise_error "unexpected end of file"
+      else if len = (read : int) then
+        Bytes.unsafe_to_string b
+      else
+        Bytes.sub_string b 0 read
+    | Tar.Really_read len ->
+      let rec loop fd buf offset len =
+        if offset < (len : int) then
+          let n = safe_read fd buf offset (len - offset) in
+          if n = 0 then
+            raise_error "unexpected end of file"
+          else
+            loop fd buf (offset + n) len
+      in
+      let buf = Bytes.create len in
+      loop fd buf 0 len;
+      Bytes.unsafe_to_string buf
+    | Tar.Return (Ok x) -> x
+    | Tar.Return (Error e) ->
+      raise_error "%s"
+        (match e with
+         | `Fatal e -> Format.asprintf "Fatal: %a" Tar.pp_error e
+         | `Eof -> "EOF"
+         | `Gz s -> "gz: "^s)
+    | Tar.High _ | Tar.Write _ | Tar.Seek _ -> assert false
+    | Tar.Bind (x, f) -> run fd (f (run fd x))
+  in run
 
-let fold_reg_files_aux f acc fd =
+let fold_reg_files_aux archive f acc fd =
   let go ?global:_ hdr acc =
     match hdr.Tar.Header.link_indicator with
     | Normal ->
@@ -74,12 +79,12 @@ let fold_reg_files_aux f acc fd =
     | LongLink -> failwith "longlinks unsupported"
     | LongName -> failwith "longnames unsupported"
   in
-  run fd (Tar_gz.in_gzipped (Tar.fold go acc))
+  run archive fd (Tar_gz.in_gzipped (Tar.fold go acc))
 
 let fold_reg_files f acc archive =
   let fd = Unix.openfile (OpamFilename.to_string archive) [Unix.O_RDONLY] 0 in
   Fun.protect ~finally:(fun () -> Unix.close fd) @@ fun () ->
-  fold_reg_files_aux f acc fd
+  fold_reg_files_aux archive f acc fd
 
 module Inplace = struct
   module Map = OpamFilename.Unix.Map
@@ -95,7 +100,8 @@ module Inplace = struct
     f {
       archive = archive;
       fd;
-      content = fold_reg_files_aux (fun acc k x -> Map.add k x acc) Map.empty fd
+      content =
+        fold_reg_files_aux archive (fun acc k x -> Map.add k x acc) Map.empty fd
     }
 
   let fold_reg_files f acc t =
@@ -136,8 +142,8 @@ module Inplace = struct
           assert false
         | Tar.Return (Ok value) ->
           value
-        | Tar.Return (Error _) ->
-          failwith "something went wrong"
+        | Tar.Return (Error e) ->
+          raise_error t.archive "%s" (match e with | `Msg e -> e)
         | Tar.Bind (x, f) ->
           run buf (f (run buf x))
       in
