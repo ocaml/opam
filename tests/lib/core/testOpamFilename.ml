ec0fc2a50a5358f899d93e3bca3df5191b326c80
Raja Boujbel <raja.boujbel@ocamlpro.com>
2026-06-24 20:08:26 +0200

    test: add test cases with cwd & parent dirname

--- tests/lib/core/testOpamFilename.ml
+++ tests/lib/core/testOpamFilename.ml
@@ -30,6 +30,7 @@ let test_starts_with ~ctxt () =
     (fun (on, prefix_dir, file, expected) ->
        test ~on ~prefix_dir ~file ~expected)
     [
+      (* absolute path without parent dirname *)
       ["Unix"; "Cygwin"], "/no/match", "/foo/bar/file", false;
       ["Unix"; "Cygwin"], "/foo/bar", "/foo/bar/file", true;
       ["Unix"; "Cygwin"], "/foo/bar", "/foo/bar/baz/file", true;
@@ -45,7 +46,35 @@ let test_starts_with ~ctxt () =
       ["Win32"], {|C:\foo\bar|}, {|C:\foo\bar-baz\file|}, false;
       ["Win32"], {|C:\foo\bar\|}, {|C:\foo\bar-baz\file|}, false;
       ["Win32"], {|C:\foo\bar\file|}, {|C:\foo\bar\file|}, false;
-      ["Win32"], {|C:\foo/bar|}, {|C:/foo\bar\file|}, true;
+      ["Win32"], {|C:\foo\bar|}, {|C:\foo\bar/file|}, true;
+      (* from current directory *)
+      (* Note that those tests exhibit how starts_with behaves with [.]
+         and [..] when constructed with OpamFilename.raw rather than specify
+         what the ideal behaviour is. *)
+      ["Unix"; "Cygwin"], "/no/match", "./foo/bar/file", false;
+      ["Unix"; "Cygwin"], "/foo/bar", "./foo/bar/file", false;
+      ["Unix"; "Cygwin"], "./foo/bar", "./foo/bar/file", true;
+      ["Win32"], {|C:\no\match|}, {|.\foo\bar\file|}, false;
+      ["Win32"], {|C:\foo\bar|}, {|.\foo\bar\file|}, false;
+      ["Win32"], {|.\foo\bar|}, {|.\foo\bar\file|}, true;
+      (* current directory in the middle *)
+      ["Unix"; "Cygwin"], "/foo/bar", "/foo/./bar/file", false;
+      ["Unix"; "Cygwin"], "/foo/bar", "/foo/bar/./file", true;
+      ["Unix"; "Cygwin"], "/foo/./bar", "/foo/./bar/file", true;
+      ["Unix"; "Cygwin"], "/foo/./bar", "/foo/./bar/./file", true;
+      ["Win32"], {|C:\foo\bar|}, {|C:\foo\.\bar\file|}, false;
+      ["Win32"], {|C:\foo\bar|}, {|C:\foo\bar\.\file|}, true;
+      ["Win32"], {|C:\foo\.\bar|}, {|C:\foo\.\bar\file|}, true;
+      ["Win32"], {|C:\foo\.\bar|}, {|C:\foo\.\bar\.\file|}, true;
+      (* absolute path with parent dirname *)
+      ["Unix"; "Cygwin"], "/foo/bar", "/foo/../bar/file", false;
+      ["Unix"; "Cygwin"], "/foo/bar", "/foo/bar/../file", true;
+      ["Unix"; "Cygwin"], "/foo/../bar", "/foo/../bar/file", true;
+      ["Unix"; "Cygwin"], "/foo/../bar", "/foo/../bar/../file", true;
+      ["Win32"], {|C:\foo\bar|}, {|C:\foo\..\bar\file|}, false;
+      ["Win32"], {|C:\foo\bar|}, {|C:\foo\bar\..\file|}, true;
+      ["Win32"], {|C:\foo\..\bar|}, {|C:\foo\..\bar\file|}, true;
+      ["Win32"], {|C:\foo\..\bar|}, {|C:\foo\..\bar\..\file|}, true;
     ]
 
 let test_dir_starts_with ~ctxt () =
@@ -70,6 +99,7 @@ let test_dir_starts_with ~ctxt () =
     (fun (on, prefix_dir, dir, expected) ->
        test ~on ~prefix_dir ~dir ~expected)
     [
+      (* absolute path without parent dirname *)
       ["Unix"; "Cygwin"], "/no/match", "/foo/bar/dir", false;
       ["Unix"; "Cygwin"], "/foo/bar", "/foo/bar/dir", true;
       ["Unix"; "Cygwin"], "/foo/bar", "/foo/bar/baz/dir", true;
@@ -85,7 +115,35 @@ let test_dir_starts_with ~ctxt () =
       ["Win32"], {|C:\foo\bar|}, {|C:\foo\bar-baz\dir|}, false;
       ["Win32"], {|C:\foo\bar\|}, {|C:\foo\bar-baz\dir|}, false;
       ["Win32"], {|C:\foo\bar\dir|}, {|C:\foo\bar\dir|}, true;
-      ["Win32"], {|C:\foo/bar|}, {|C:/foo\bar\dir|}, true;
+      ["Win32"], {|C:\foo\bar|}, {|C:\foo\bar/dir|}, true;
+      (* from current directory *)
+      (* Note that those tests exhibit how dir_starts_with behaves with [.]
+         and [..] when constructed with OpamFilename.raw rather than specify
+         what the ideal behaviour is. *)
+      ["Unix"; "Cygwin"], "/no/match", "./foo/bar/dir", false;
+      ["Unix"; "Cygwin"], "/foo/bar", "./foo/bar/dir", false;
+      ["Unix"; "Cygwin"], "./foo/bar", "./foo/bar/dir", true;
+      ["Win32"], {|C:\no\match|}, {|.\foo\bar\dir|}, false;
+      ["Win32"], {|C:\foo\bar|}, {|.\foo\bar\dir|}, false;
+      ["Win32"], {|.\foo\bar|}, {|.\foo\bar\dir|}, true;
+      (* current directory in the middle *)
+      ["Unix"; "Cygwin"], "/foo/bar", "/foo/./bar/dir", false;
+      ["Unix"; "Cygwin"], "/foo/bar", "/foo/bar/./dir", true;
+      ["Unix"; "Cygwin"], "/foo/./bar", "/foo/./bar/dir", true;
+      ["Unix"; "Cygwin"], "/foo/./bar", "/foo/./bar/./dir", true;
+      ["Win32"], {|C:\foo\bar|}, {|C:\foo\.\bar\dir|}, false;
+      ["Win32"], {|C:\foo\bar|}, {|C:\foo\bar\.\dir|}, true;
+      ["Win32"], {|C:\foo\.\bar|}, {|C:\foo\.\bar\dir|}, true;
+      ["Win32"], {|C:\foo\.\bar|}, {|C:\foo\.\bar\.\dir|}, true;
+      (* absolute path with parent dirname *)
+      ["Unix"; "Cygwin"], "/foo/bar", "/foo/../bar/dir", false;
+      ["Unix"; "Cygwin"], "/foo/bar", "/foo/bar/../dir", true;
+      ["Unix"; "Cygwin"], "/foo/../bar", "/foo/../bar/dir", true;
+      ["Unix"; "Cygwin"], "/foo/../bar", "/foo/../bar/../dir", true;
+      ["Win32"], {|C:\foo\bar|}, {|C:\foo\..\bar\dir|}, false;
+      ["Win32"], {|C:\foo\bar|}, {|C:\foo\bar\..\dir|}, true;
+      ["Win32"], {|C:\foo\..\bar|}, {|C:\foo\..\bar\dir|}, true;
+      ["Win32"], {|C:\foo\..\bar|}, {|C:\foo\..\bar\..\dir|}, true;
     ]
 
 let test ~ctxt () =
